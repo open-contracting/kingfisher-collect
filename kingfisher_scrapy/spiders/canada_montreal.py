ce08725f79157e1e7b61866139c1a531ce19fcf9
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2019-01-18 15:23:59 -0300

    Add Canada Montreal Spider
    Signed-off-by: Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>

---
+++ kingfisher_scrapy/spiders/canada_montreal.py
@@ -0,0 +1,25 @@
+import json
+
+import scrapy
+
+
+class CanadaMontreal(scrapy.Spider):
+    name = 'canada_montreal'
+    start_urls = ['https://ville.montreal.qc.ca/vuesurlescontrats/api/releases.json?limit=1']
+
+    def parse(self, response):
+        sample = hasattr(self, 'sample') and self.sample == 'true'
+        data = json.loads(response.body_as_unicode())
+        total = data['meta']['count']
+        offset = 0
+        limit = 10000
+        if sample:
+            total = 1
+            limit = 50
+        while offset < total:
+            yield {
+                'file_urls': ['https://ville.montreal.qc.ca/vuesurlescontrats/api/releases.json?limit=%d&offset=%d' %
+                              (limit, offset)],
+                'data_type': 'release_package',
+            }
+            offset += limit
