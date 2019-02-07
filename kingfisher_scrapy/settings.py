0c4ec37f03c798864ecf123351b4785aa6b738a2
Rob Redpath <rob.redpath@opendataservices.coop>
2019-02-07 14:22:25 +0000

    flake8

--- kingfisher_scrapy/settings.py
+++ kingfisher_scrapy/settings.py
@@ -29,7 +29,7 @@ CONCURRENT_REQUESTS = 32
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
 # DOWNLOAD_DELAY = 3
-# The download delay setting will honor only one of: 
+# The download delay setting will honor only one of:
 CONCURRENT_REQUESTS_PER_DOMAIN = 2
 # CONCURRENT_REQUESTS_PER_IP = 16
 
