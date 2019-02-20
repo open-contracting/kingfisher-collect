c06ebc2d2e1050324ca4dcab080620fe2d954886
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2019-02-20 14:33:01 -0300

    Handle Colombia Special case
    Signed-off-by: Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>

--- kingfisher_scrapy/spiders/colombia.py
+++ kingfisher_scrapy/spiders/colombia.py
@@ -1,5 +1,8 @@
 import json
+from json import JSONDecodeError
+
 import scrapy
+import time
 
 from kingfisher_scrapy.base_spider import BaseSpider
 
@@ -9,12 +12,17 @@ class Colombia(BaseSpider):
     start_urls = ['https://apiocds.colombiacompra.gov.co:8443/apiCCE2.0/rest/releases?page=1']
 
     def parse(self, response):
-        json_data = json.loads(response.body_as_unicode())
-        if not (hasattr(self, 'sample') and self.sample == 'true'):
-            if 'links' in json_data and 'next' in json_data['links']:
-                yield scrapy.Request(json_data['links']['next'])
+        try:
+            json_data = json.loads(response.body_as_unicode())
+            if not self.is_sample():
+                if 'links' in json_data and 'next' in json_data['links']:
+                    yield scrapy.Request(json_data['links']['next'])
 
-        yield {
-            'file_urls': [response.url],
-            'data_type': 'release_package'
-        }
+            yield {
+                'file_urls': [response.url],
+                'data_type': 'release_package'
+            }
+
+        except JSONDecodeError:
+            time.sleep(120*60)
+            yield scrapy.Request(response.url)
