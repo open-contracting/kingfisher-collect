0d5fcee8ad1a68304de23c16007ececbab8cdad0
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2019-02-21 11:18:26 -0300

    Update colombia spider with more doc and cases
    Signed-off-by: Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>

--- kingfisher_scrapy/spiders/colombia.py
+++ kingfisher_scrapy/spiders/colombia.py
@@ -7,12 +7,27 @@ import time
 from kingfisher_scrapy.base_spider import BaseSpider
 
 
+def wait_and_retry(url):
+    time.sleep(120 * 60)
+    yield scrapy.Request(url)
+
+
 class Colombia(BaseSpider):
     name = 'colombia'
     start_urls = ['https://apiocds.colombiacompra.gov.co:8443/apiCCE2.0/rest/releases?page=1']
+    handle_httpstatus_list = [503]
 
     def parse(self, response):
+        # In Colombia, every day at certain hour they run a process in their system that drops the database and make
+        # the services unavailable for about 120 minutes, as Colombia has a lot of data,
+        # the spider takes more than one day to scrape all the data,
+        # so eventually the spider will always face the service problems. For that, when the problem occurs, (503
+        # status or invalid json) we wait 120 minutes and then continue
         try:
+
+            if response.status == 503:
+                wait_and_retry(response.url)
+
             json_data = json.loads(response.body_as_unicode())
             if not self.is_sample():
                 if 'links' in json_data and 'next' in json_data['links']:
@@ -24,5 +39,4 @@ class Colombia(BaseSpider):
             }
 
         except JSONDecodeError:
-            time.sleep(120*60)
-            yield scrapy.Request(response.url)
+            wait_and_retry(response.url)
