9355b63ec241cb483d6207e7b962206cab65f620
James Baster <james.baster@opendataservices.coop>
2019-06-18 15:02:32 +0100

    Australia NSW scraper - higher page limit and new system
    * limit of 10 is no longer is effect, so let's raise to 1000
    * port to new system for faster working

--- kingfisher_scrapy/spiders/australia_nsw.py
+++ kingfisher_scrapy/spiders/australia_nsw.py
@@ -1,61 +1,91 @@
 import json
 import scrapy
+import hashlib
 
 from kingfisher_scrapy.base_spider import BaseSpider
 
 
-# This Spider uses the old system of pipelines! DO NOT USE IT AS AN EXAMPLE OF WHAT TO DO IN FUTURE SPIDERS!
-# Thank you.
 class AustraliaNSW(BaseSpider):
     name = 'australia_nsw'
+    start_urls = ['https://tenders.nsw.gov.au']
     custom_settings = {
         'ITEM_PIPELINES': {
-            'kingfisher_scrapy.pipelines.OldKingfisherFilesPipeline': 400,
-            'kingfisher_scrapy.pipelines.OldKingfisherPostPipeline': 800,
-        }
+            'kingfisher_scrapy.pipelines.KingfisherPostPipeline': 400
+        },
+        'HTTPERROR_ALLOW_ALL': True,
     }
 
     def start_requests(self):
-        sample = hasattr(self, 'sample') and self.sample == 'true'
         release_types = ['planning', 'tender', 'contract']
-        url = 'https://tenders.nsw.gov.au'
-        url += '/?event=public.api.%s.search&ResultsPerPage=10'
-        if sample:
-            release_types = ['planning']
-        for r in release_types:
-            yield scrapy.Request(url % r, meta={'data_type': 'meta', 'release_type': r})
+        page_limit = 10 if self.is_sample() else 1000
+        url = 'https://tenders.nsw.gov.au/?event=public.api.{}.search&ResultsPerPage={}'
+        for release_type in release_types:
+            yield scrapy.Request(
+                url.format(release_type, page_limit),
+                meta={'release_type': release_type},
+                callback=self.parse_list
+            )
 
-    def parse(self, response):
-        sample = hasattr(self, 'sample') and self.sample == 'true'
-        json_data = json.loads(response.body_as_unicode())
-        data_type = response.meta['data_type']
-        release_type = response.meta['release_type']
-        if data_type == 'meta':
-            stage_urls = []
-            if 'links' in json_data and 'next' in json_data['links'] and not sample:
-                stage_urls.append(json_data['links']['next'])
+    def parse_list(self, response):
+        if response.status == 200:
 
+            json_data = json.loads(response.body_as_unicode())
+
+            # More Pages?
+            if 'links' in json_data and 'next' in json_data['links'] and not self.is_sample():
+                yield scrapy.Request(
+                    json_data['links']['next'],
+                    meta={'release_type': response.request.meta['release_type']},
+                    callback=self.parse_list
+                )
+
+            # Data?
             for release in json_data['releases']:
-                if release_type == 'planning':
+                if response.request.meta['release_type'] == 'planning':
                     uuid = release['tender']['plannedProcurementUUID']
-                    stage_urls.append('https://tenders.nsw.gov.au/?event=public.api.planning.view'
-                                      '&PlannedProcurementUUID=%s' % uuid)
-                if release_type == 'tender':
+                    yield scrapy.Request(
+                        'https://tenders.nsw.gov.au/?event=public.api.planning.view&PlannedProcurementUUID=%s' % uuid,
+                        meta={'kf_filename': 'plannning-%s.json' % uuid},
+                        callback=self.parse
+                    )
+                if response.request.meta['release_type'] == 'tender':
                     uuid = release['tender']['RFTUUID']
-                    stage_urls.append('https://tenders.nsw.gov.au/?event=public.api.tender.view&RFTUUID=%s' % uuid)
-                if release_type == 'contract':
+                    yield scrapy.Request(
+                        'https://tenders.nsw.gov.au/?event=public.api.tender.view&RFTUUID=%s' % uuid,
+                        meta={'kf_filename': 'tender-%s.json' % uuid},
+                        callback=self.parse
+                    )
+                if response.request.meta['release_type'] == 'contract':
                     for award in release['awards']:
                         uuid = award['CNUUID']
-                        stage_urls.append('https://tenders.nsw.gov.au/?event=public.api.contract.view&CNUUID=%s'
-                                          % uuid)
-            if sample:
-                stage_urls = [stage_urls[0]]
+                        yield scrapy.Request(
+                            'https://tenders.nsw.gov.au/?event=public.api.contract.view&CNUUID=%s' % uuid,
+                            meta={'kf_filename': 'contract-%s.json' % uuid},
+                            callback=self.parse
+                        )
 
-            for url in stage_urls:
-                yield scrapy.Request(url, meta={'data_type': 'release_package', 'release_type': release_type})
         else:
             yield {
-                'file_urls': [response.url],
-                'data_type': 'release_package',
-                'release_type': release_type
+                'success': False,
+                'file_name': hashlib.md5(response.request.url.encode('utf-8')).hexdigest() + '.json',
+                "url": response.request.url,
+                "errors": {"http_code": response.status}
+            }
+
+    def parse(self, response):
+        if response.status == 200:
+            self.save_response_to_disk(response, response.request.meta['kf_filename'])
+            yield {
+                'success': True,
+                'file_name': response.request.meta['kf_filename'],
+                "data_type": "release_package",
+                "url": response.request.url,
+            }
+
+        else:
+            yield {
+                'success': False,
+                'file_name': response.request.meta['kf_filename'],
+                "url": response.request.url,
+                "errors": {"http_code": response.status}
             }
