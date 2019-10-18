a379108453fc7a041604b638527f332320c7f790
James Baster <james.baster@opendataservices.coop>
2019-10-18 16:30:15 +0100

    Add UK FTS scraper https://github.com/open-contracting/kingfisher-scrape/issues/205

---
+++ kingfisher_scrapy/spiders/uk_fts.py
@@ -0,0 +1,48 @@
+import json
+import hashlib
+
+import scrapy
+
+from kingfisher_scrapy.base_spider import BaseSpider
+
+
+class UKContractsFinder(BaseSpider):
+    name = 'uk_fts'
+    custom_settings = {
+        'ITEM_PIPELINES': {
+            'kingfisher_scrapy.pipelines.KingfisherPostPipeline': 400
+        },
+        'HTTPERROR_ALLOW_ALL': True,
+    }
+
+    def start_requests(self):
+        yield scrapy.Request(
+            url='https://enoticetest.service.xgov.uk/api/1.0/ocdsReleasePackages',
+            meta={'kf_filename': 'start.json'},
+            headers={'Accept': 'application/json'}
+        )
+
+    def parse(self, response):
+
+        if response.status == 200:
+
+            yield self.save_response_to_disk(
+                response,
+                response.request.meta['kf_filename'],
+                data_type='release_package_in_ocdsReleasePackage_in_list_in_results'
+            )
+
+            json_data = json.loads(response.body_as_unicode())
+            if not self.is_sample() and json_data['nextCursor']:
+                yield scrapy.Request(
+                    url="https://enoticetest.service.xgov.uk/api/1.0/ocdsReleasePackages?cursor=" + json_data['nextCursor'],
+                    meta={'kf_filename': hashlib.md5(json_data['nextCursor'].encode('utf-8')).hexdigest() + '.json'},
+                    headers={'Accept': 'application/json'}
+                )
+        else:
+            yield {
+                'success': False,
+                'file_name': response.request.meta['kf_filename'],
+                'url': response.request.url,
+                'errors': {'http_code': response.status}
+            }
