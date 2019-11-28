3b3d5b3091fc09b6e16309e0264efb19d79a9565
Yohanna Lisnichuk <ylisnichuk@dncp.gov.py>
2019-11-28 08:34:53 -0300

    Update chile compra Scrapers
    - Created a chile_base class for encapsulate the logic
    - Update the scrapers to handle the ocid list service changes:
      - pagination, key changes
    
    Signed-off-by: Yohanna Lisnichuk <ylisnichuk@dncp.gov.py>

--- kingfisher_scrapy/spiders/chile_compra_releases.py
+++ kingfisher_scrapy/spiders/chile_compra_releases.py
@@ -1,59 +1,13 @@
-import datetime
-import json
-
-import scrapy
-
-from kingfisher_scrapy.base_spider import BaseSpider
+from kingfisher_scrapy.spiders.chile_base import ChileCompraBaseSpider
 
 
-class ChileCompraReleases(BaseSpider):
+class ChileCompraReleases(ChileCompraBaseSpider):
     name = 'chile_compra_releases'
-    custom_settings = {
-        'ITEM_PIPELINES': {
-            'kingfisher_scrapy.pipelines.KingfisherPostPipeline': 400
-        },
-        'HTTPERROR_ALLOW_ALL': True,
-    }
-    # the data list service takes too long to be downloaded, so we increase the download timeout
-    download_timeout = 300
-
-    def start_requests(self):
-        if self.is_sample():
-            yield scrapy.Request(
-                url='https://apis.mercadopublico.cl/OCDS/data/listaA%C3%B1oMes/2017/10',
-                meta={'kf_filename': 'sample.json'}
-            )
-            return
-        current_year = datetime.datetime.now().year + 1
-        current_month = datetime.datetime.now().month
-        for year in range(2008, current_year):
-            for month in range(1, 13):
-                if current_year == year and month > current_month:
-                    break
-                yield scrapy.Request(
-                    url='https://apis.mercadopublico.cl/OCDS/data/listaA%C3%B1oMes/{}/{:02d}'.format(year, month),
-                    meta={'kf_filename': 'year-{}-month-{:02d}.json'.format(year, month)}
-                )
 
     def parse(self, response):
         if response.status == 200:
-            data = json.loads(response.body_as_unicode())
-            if 'NumeroError' in data:
-                yield scrapy.Request(
-                    url=response.request.url,
-                    meta={'kf_filename': response.request.meta['kf_filename']}
-                )
-            elif 'ListadoOCDS' in data:
-                for data_item in data['ListadoOCDS']:
-                    for stage in list(data_item.keys()):
-                        if 'URL' in stage:
-                            name = stage.replace('URL', '')
-                            yield scrapy.Request(
-                                url=data_item[stage],
-                                meta={'kf_filename': 'data-%s-%s.json' % (data_item['Codigo'], name)}
-                            )
-            else:
-                yield self.save_response_to_disk(response, response.request.meta['kf_filename'], data_type='release_package')
+            for item in self.base_parse(response, 'release'):
+                yield item
         else:
             yield {
                 'success': False,
