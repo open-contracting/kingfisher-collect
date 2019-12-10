e7fda827bbcaee190dfcf18bde2a12f186c50a19
Andres Aguilera <amaguilera96@gmail.com>
2019-12-10 15:36:31 -0300

    Fix calculated value

--- kingfisher_scrapy/spiders/honduras_portal_releases.py
+++ kingfisher_scrapy/spiders/honduras_portal_releases.py
@@ -6,7 +6,6 @@ from kingfisher_scrapy.base_spider import BaseSpider
 class HondurasPortalReleases(BaseSpider):
     name = 'honduras_portal_releases'
     download_delay = 1.5
-    final = '107996'
     custom_settings = {
         'ITEM_PIPELINES': {
             'kingfisher_scrapy.pipelines.KingfisherPostPipeline': 400
@@ -15,9 +14,6 @@ class HondurasPortalReleases(BaseSpider):
     }
 
     def start_requests(self):
-        if self.is_sample():
-            self.final = '11'
-
         yield scrapy.Request(
             url='http://200.13.162.86/api/v1/release/?format=json',
             meta={'kf_filename': 'page1.json'}
@@ -35,7 +31,7 @@ class HondurasPortalReleases(BaseSpider):
             )
 
             url = json_data['next']
-            if not url or url[54:] == self.final:
+            if not url or self.is_sample():
                 return
             else:
                 yield scrapy.Request(
