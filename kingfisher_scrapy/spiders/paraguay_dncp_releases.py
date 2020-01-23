6cabb5dd82acfd4e369cdf6eb1b91d2b22e9c557
Andres Aguilera <amaguilera96@gmail.com>
2020-01-23 14:46:13 -0300

    Update kf_filename and url parse in Paraguay scrapers

--- kingfisher_scrapy/spiders/paraguay_dncp_releases.py
+++ kingfisher_scrapy/spiders/paraguay_dncp_releases.py
@@ -1,7 +1,5 @@
-import re
-
 from kingfisher_scrapy.spiders.paraguay_base import ParaguayDNCPBaseSpider
-
+from urllib.parse import urlparse
 
 class ParaguayDNCPReleases(ParaguayDNCPBaseSpider):
     name = 'paraguay_dncp_releases'
@@ -10,10 +8,11 @@ class ParaguayDNCPReleases(ParaguayDNCPBaseSpider):
     def get_files_to_download(self, content):
         for record in content['records']:
             for release in record['releases']:
-                match = re.search('[^/]+$', release['url'])
-                if match:
-                    url = 'http://beta.dncp.gov.py/datos/api/v3/doc/ocds/releases/id/{}'.format(match.group(0))
-                    file_name = '{}.json'.format(match.group(0))
-                    yield url, file_name
+                parsed = urlparse(release['url'])
+                path = parsed.path.lstrip("/datos/api/v3/doc/")
+                id = path.lstrip("releases")
+                if id:
+                    url = 'http://beta.dncp.gov.py/datos/api/v3/doc/ocds/releases/id{}'.format(id)
+                    yield url
                 else:
                     raise Exception('Release ID not found')
