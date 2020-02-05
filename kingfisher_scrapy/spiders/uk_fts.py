617e22f4ce0fb4519ace1f87b089f2d277f9fe89
Andres Aguilera <amaguilera96@gmail.com>
2020-02-05 00:00:54 -0300

    Update uk_ftp scraper with LinksSpider

--- kingfisher_scrapy/spiders/uk_fts.py
+++ kingfisher_scrapy/spiders/uk_fts.py
@@ -1,12 +1,9 @@
-import hashlib
-import json
-
 import scrapy
 
-from kingfisher_scrapy.base_spider import BaseSpider
+from kingfisher_scrapy.base_spider import BaseSpider, LinksSpider
 
 
-class UKContractsFinder(BaseSpider):
+class UKContractsFinder(BaseSpider, LinksSpider):
     name = 'uk_fts'
 
     def start_requests(self):
@@ -26,13 +23,8 @@ class UKContractsFinder(BaseSpider):
                 data_type='release_package_in_ocdsReleasePackage_in_list_in_results'
             )
 
-            json_data = json.loads(response.body_as_unicode())
-            if not self.is_sample() and json_data['nextCursor']:
-                yield scrapy.Request(
-                    url="https://enoticetest.service.xgov.uk/api/1.0/ocdsReleasePackages?cursor=" + json_data['nextCursor'],
-                    meta={'kf_filename': hashlib.md5(json_data['nextCursor'].encode('utf-8')).hexdigest() + '.json'},
-                    headers={'Accept': 'application/json'}
-                )
+            if not self.is_sample():
+                yield self.next_link(response)
         else:
             yield {
                 'success': False,
