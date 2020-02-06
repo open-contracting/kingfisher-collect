6214f02746e49f250181a28f6df511c48cd5138e
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-02-06 16:39:45 -0500

    flake8 setup.py and copy-edit comments.

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup
 
-# Note this project is not intended to be released or used as a library!
-# But because of how we deploy these scrapers into scrapyd, we need a setup.py file.
+# This project is not intended to be released or used as a Python package. This file only exists for scrapyd-client.
+# https://github.com/scrapy/scrapyd-client/blob/v1.1.0/README.rst
 
 setup(
     packages=[
         'kingfisher_scrapy',
         'kingfisher_scrapy.spiders',
     ],
-    entry_points={'scrapy': ['settings = kingfisher_scrapy.settings']},
+    entry_points={
+        'scrapy': [
+            'settings = kingfisher_scrapy.settings',
+        ],
+    },
 )
