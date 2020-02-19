d9b973e009831ef7ec41250ab443bb0e53a1536e
Andres Aguilera <amaguilera96@gmail.com>
2020-02-19 14:24:45 -0300

    Use settings

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -3,3 +3,5 @@ import kingfisher_scrapy.settings
 
 class TestSettings():
     name = 'test'
+    both_name = kingfisher_scrapy.settings.BOT_NAME
+    assert both_name == kingfisher_scrapy.settings.BOT_NAME
