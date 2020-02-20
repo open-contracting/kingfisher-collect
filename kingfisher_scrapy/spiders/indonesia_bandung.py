44894c973174e90556198935b5ca63ec9d49b19d
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2020-02-20 13:45:18 -0300

    Update indonesia spider
    Signed-off-by: Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>

--- kingfisher_scrapy/spiders/indonesia_bandung.py
+++ kingfisher_scrapy/spiders/indonesia_bandung.py
@@ -12,7 +12,7 @@ class IndonesiaBandung(BaseSpider):
 
     def start_requests(self):
         url = 'https://birms.bandung.go.id/api/packages/year/{}'
-        current_year = datetime.datetime.now().year
+        current_year = datetime.datetime.now().year + 1
         for year in range(2013, current_year):
             yield scrapy.Request(
                 url.format(year),
@@ -22,9 +22,8 @@ class IndonesiaBandung(BaseSpider):
 
     def parse_data(self, response):
         if response.status == 200:
-            json_data = json.loads(response.text)
-            datas = json_data.get('data')
-            for data in datas:
+            json_data = json.loads(response.text).get('data')
+            for data in json_data:
                 url = data.get('uri')
                 if url:
                     yield scrapy.Request(
@@ -45,7 +44,7 @@ class IndonesiaBandung(BaseSpider):
                 'success': False,
                 'file_name': response.request.meta['kf_filename'],
                 'url': response.request.url,
-                'errors': {"http_code": response.status}
+                'errors': {'http_code': response.status}
             }
 
     def parse(self, response):
@@ -56,12 +55,12 @@ class IndonesiaBandung(BaseSpider):
             yield self.save_response_to_disk(
                 response,
                 response.request.meta['kf_filename'],
-                data_type="release"
+                data_type='release'
             )
         else:
             yield {
                 'success': False,
                 'file_name': response.request.meta['kf_filename'],
                 'url': response.request.url,
-                'errors': {"http_code": response.status}
+                'errors': {'http_code': response.status}
             }
