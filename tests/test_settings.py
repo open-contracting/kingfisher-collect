35b6b4e46f6893a6b2fd5b934c980225e3310a69
Andres Aguilera <amaguilera96@gmail.com>
2020-02-24 09:04:13 -0300

    Delete test file

--- tests/test_settings.py
+++
@@ -1,7 +0,0 @@
-import kingfisher_scrapy.settings
-
-
-class TestSettings():
-    name = 'test'
-    both_name = kingfisher_scrapy.settings.BOT_NAME
-    assert both_name == kingfisher_scrapy.settings.BOT_NAME
