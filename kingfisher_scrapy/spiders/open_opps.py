e2a37f0b3f54441e64eff81b4ba189cfaff8f9ba
Andres Aguilera <amaguilera96@gmail.com>
2020-03-04 14:23:18 -0300

    Change data type and add more comments

--- kingfisher_scrapy/spiders/open_opps.py
+++ kingfisher_scrapy/spiders/open_opps.py
@@ -57,7 +57,7 @@ class OpenOpps(BaseSpider):
             if token:
                 self.logger.info('New access token: {}'.format(token))
                 self.access_token = 'JWT ' + token
-                """ Start """
+                """ Start requests """
                 return self.start_requests_pages()
             else:
                 self.logger.error(
@@ -69,7 +69,7 @@ class OpenOpps(BaseSpider):
             raise AuthenticationFailureException()
 
     def start_requests_pages(self):
-        # Use larger ranges for filters with less than (api_limit) search results
+        # use larger ranges for filters with less than (api_limit) search results
         if not self.sample:
             release_date_gte_list = ['', '2009-01-01', '2010-01-01', '2010-07-01']
             release_date_lte_list = ['2008-12-31', '2009-12-31', '2010-06-30', '2010-12-31']
@@ -86,7 +86,7 @@ class OpenOpps(BaseSpider):
                     headers={"Accept": "*/*", "Content-Type": "application/json"},
                 )
 
-        # Use smaller ranges for filters with more than (api_limit) search results
+        # use smaller ranges (day by day) for filters with more than (api_limit) search results
         for year in range(2011, datetime.now().year + 1):
             start_date = datetime(year, 1, 1)
             end_date = datetime(year, datetime.now().month, datetime.now().day) \
@@ -120,25 +120,37 @@ class OpenOpps(BaseSpider):
             count = results.get('count')
             if count <= 10000:
 
+                # data type changed to release package list in order to have fewer files
+                all_data = {}
                 for data in results.get('results'):
                     json_data = data.get('json')
-                    if not json_data:
-                        continue
-                    json_uri = json_data.get('uri')
-                    if not json_uri:
-                        json_uri = response.request.url + data.get('ocid')
+                    if json_data:
+                        all_data.update({len(all_data): json_data})
+
+                if all_data:
                     self.save_data_to_disk(
-                        json_data,
-                        filename=hashlib.md5(json_uri.encode('utf-8')).hexdigest() + '.json',
-                        url=json_uri,
-                        data_type='release_package'
+                        all_data,
+                        filename=hashlib.md5(response.request.url.encode('utf-8')).hexdigest() + '.json',
+                        url=response.request.url,
+                        data_type='release_package_list'
+                    )
+                    if self.sample:
+                        return
+
+                next_url = results.get('next')
+                if next_url:
+                    yield scrapy.Request(
+                        url=next_url,
+                        headers={"Accept": "*/*", "Content-Type": "application/json"},
                     )
             else:
                 if 'release_date' in response.request.meta and response.request.meta['release_date']:
-                    parts = int(ceil(count / self.api_limit))  # parts we split a day with more than 10K data
+                    parts = int(ceil(count / self.api_limit))  # parts we split a search that exceeds the limit
                     split_h = int(ceil(24 / parts))  # hours we split
                     date = response.request.meta['release_date']
                     date = datetime.strptime(date, "%Y-%m-%d")
+
+                    # create time lists depending on how many hours we split a day
                     start_hour_list = [(date + timedelta(hours=H)
                                         ).strftime("%Y-%m-%dT%H:%M:%S") for H in range(0, 24, split_h)]
                     end_hour_list = [(date + timedelta(hours=H, minutes=59, seconds=59)
@@ -161,18 +173,9 @@ class OpenOpps(BaseSpider):
                             headers={"Accept": "*/*", "Content-Type": "application/json"},
                             meta={"release_date": date},
                         )
-
-            if not self.sample:
-                next_url = results.get('next')
-                if next_url:
-                    yield scrapy.Request(
-                        url=next_url,
-                        headers={"Accept": "*/*", "Content-Type": "application/json"},
-                    )
         else:
-            if response.status != 500:
-                yield {
-                    'success': False,
-                    'url': response.request.url,
-                    'errors': {'http_code': response.status}
-                }
+            yield {
+                'success': False,
+                'url': response.request.url,
+                'errors': {'http_code': response.status}
+            }
