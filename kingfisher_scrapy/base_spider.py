f07793650be08556711d5fd1a3052aac87f1634f
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-04-28 14:22:14 -0400

    Fix bug introduced in 8d7fdf0 #357, due to removal of keywords for keyword arguments, closes #372

--- kingfisher_scrapy/base_spider.py
+++ kingfisher_scrapy/base_spider.py
@@ -191,7 +191,7 @@ class ZipSpider(BaseSpider):
                 if file_format == 'json_lines':
                     yield from self.parse_json_lines(data, data_type, response.request.url, encoding=encoding)
                 else:
-                    yield self.save_data_to_disk(data.read(), filename, data_type, response.request.url,
+                    yield self.save_data_to_disk(data.read(), filename, data_type=data_type, url=response.request.url,
                                                  encoding=encoding)
         else:
             yield {
