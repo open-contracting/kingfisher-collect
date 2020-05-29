b2d0b88ca49540b552d93241d1d4605956f78a6c
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-05-29 18:29:27 -0400

    Add test for Validate item pipeline

---
+++ tests/test_validate.py
@@ -0,0 +1,24 @@
+import pytest
+from kingfisher_scrapy.exceptions import MissingRequiredFieldError
+from kingfisher_scrapy.items import File
+from kingfisher_scrapy.pipelines import Validate
+
+
+def test_process_item():
+    pipeline = Validate()
+    item = File({
+        'file_name': '',
+        'data': '',
+        'data_type': '',
+        'url': '',
+    })
+
+    assert pipeline.process_item(item, None) == item
+
+
+def test_process_item_error():
+    pipeline = Validate()
+    item = File()
+
+    with pytest.raises(MissingRequiredFieldError):
+        pipeline.process_item(item, None)
