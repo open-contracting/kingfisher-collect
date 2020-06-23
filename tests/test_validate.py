f0e60060f92125bf44304af247973776424acf55
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-06-22 22:28:08 -0400

    pipelines: Don't test class internals. Add whitespace between assertions and other code.

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -26,7 +26,7 @@ def test_process_item_error():
         pipeline.process_item(item, None)
 
 
-def test_duplicated_filename(caplog):
+def test_duplicate_file(caplog):
     pipeline = Validate()
     spider = spider_with_crawler()
     item = File({
@@ -35,17 +35,18 @@ def test_duplicated_filename(caplog):
         'data_type': '',
         'url': '',
     })
-    assert pipeline.process_item(item, spider) == item
+
     pipeline.process_item(item, spider)
-    assert caplog.messages[0] == 'Duplicated filename: test1'
-    assert len(pipeline.file_names) == 1
+    pipeline.process_item(item, spider)
     item2 = item.copy()
     item2['file_name'] = 'file2'
     pipeline.process_item(item2, spider)
-    assert len(pipeline.file_names) == 2
+
+    assert len(caplog.messages) == 1
+    assert caplog.messages[0] == "Duplicate File: 'test1'"
 
 
-def test_duplicated_fileitem(caplog):
+def test_duplicate_file_item(caplog):
     pipeline = Validate()
     spider = spider_with_crawler()
     item = FileItem({
@@ -55,11 +56,12 @@ def test_duplicated_fileitem(caplog):
         'url': '',
         'number': 1
     })
-    assert pipeline.process_item(item, spider) == item
+
     pipeline.process_item(item, spider)
-    assert caplog.messages[0] == 'Duplicated filename and number pair: test1-1'
-    assert len(pipeline.file_items) == 1
+    pipeline.process_item(item, spider)
     item2 = item.copy()
     item2['number'] = 2
     pipeline.process_item(item2, spider)
-    assert len(pipeline.file_items) == 2
+
+    assert len(caplog.messages) == 1
+    assert caplog.messages[0] == "Duplicate FileItem: ('test1', 1)"
