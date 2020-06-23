d4079efc86c8c6cee870cfcd40d73b26b23422ac
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2020-06-23 09:11:16 -0400

    Add a test per item class
    Signed-off-by: Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -1,7 +1,7 @@
 import pytest
 from jsonschema import ValidationError
 
-from kingfisher_scrapy.items import File
+from kingfisher_scrapy.items import File, FileItem, FileError
 from kingfisher_scrapy.pipelines import Validate
 
 
@@ -19,7 +19,67 @@ def test_process_item():
 
 def test_process_item_error():
     pipeline = Validate()
-    item = File()
+    item = File({
+        'data': 'data',
+        'data_type': 'release_package',
+        'url': 'http://test.com',
+    })
 
+    with pytest.raises(ValidationError):
+        pipeline.process_item(item, None)
+    item['file_name'] = 'test'
+    item['data_type'] = 'not a valid data type'
+    with pytest.raises(ValidationError):
+        pipeline.process_item(item, None)
+
+
+def test_process_file_item():
+    pipeline = Validate()
+    item = FileItem({
+        'file_name': 'test',
+        'data': 'data',
+        'data_type': 'release_package',
+        'url': 'http://test.com',
+        'number': 1
+    })
+    assert pipeline.process_item(item, None) == item
+
+
+def test_process_file_item_error():
+    pipeline = Validate()
+    item = FileItem({
+        'file_name': 'test',
+        'data': 'data',
+        'data_type': 'release_package',
+        'url': 'http://test.com',
+        'number': "2"
+    })
+    with pytest.raises(ValidationError):
+        pipeline.process_item(item, None)
+    item['number'] = None
+    with pytest.raises(ValidationError):
+        pipeline.process_item(item, None)
+
+
+def test_process_file_error():
+    pipeline = Validate()
+    item = FileError({
+        'file_name': 'test',
+        'url': 'http://test.com',
+        'errors': 'Error'
+    })
+    assert pipeline.process_item(item, None) == item
+
+
+def test_process_file_item_error_error():
+    pipeline = Validate()
+    item = FileError({
+        'file_name': 'test',
+        'url': 'http://test.com'
+    })
+    with pytest.raises(ValidationError):
+        pipeline.process_item(item, None)
+    item['errors'] = 'Error'
+    item['url'] = 'not an url'
     with pytest.raises(ValidationError):
         pipeline.process_item(item, None)
