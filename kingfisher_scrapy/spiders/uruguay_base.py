37f8972d2628ca008625753ab1bc0862d00e06aa
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-07-13 15:22:05 -0400

    uruguay_base: Eliminate additional lines that duplicate logic in BaseSpider.from_crawler

--- kingfisher_scrapy/spiders/uruguay_base.py
+++ kingfisher_scrapy/spiders/uruguay_base.py
@@ -12,16 +12,14 @@ class UruguayBase(SimpleSpider):
     def from_crawler(cls, crawler, sample=None, from_date=None, until_date=None, *args, **kwargs):
         if not from_date:
             from_date = cls.default_from_date
-        if not until_date:
-            until_date = date.today().strftime('%Y-%m')
-        if sample:
-            from_date = until_date
 
         return super().from_crawler(crawler, date_format='year-month', sample=sample, from_date=from_date,
                                     until_date=until_date, *args, **kwargs)
 
     def start_requests(self):
         url = 'http://comprasestatales.gub.uy/ocds/rss/{0.year:d}/{0.month:02d}'
+        if self.sample:
+            self.from_date = self.until_date
 
         for d in date_range_by_month(self.from_date, self.until_date):
             yield self.build_request(url.format(d), formatter=components(-2), callback=self.parse_list)
