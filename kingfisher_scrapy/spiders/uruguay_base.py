02ac4c0f4729ad568b471e641884e3f40c102175
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2020-07-13 10:03:47 -0400

    Add from_date and until_date parameters to uruguay_base
    Signed-off-by: Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>

--- kingfisher_scrapy/spiders/uruguay_base.py
+++ kingfisher_scrapy/spiders/uruguay_base.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, datetime
 
 from kingfisher_scrapy.base_spider import SimpleSpider
 from kingfisher_scrapy.util import components, date_range_by_month
@@ -6,12 +6,16 @@ from kingfisher_scrapy.util import components, date_range_by_month
 
 class UruguayBase(SimpleSpider):
     download_delay = 0.9
+    default_from_date = '11-2017'
+
+    @classmethod
+    def from_crawler(cls, crawler, *args, **kwargs):
+        return super(UruguayBase, cls).from_crawler(crawler, date_format='month-year', *args, **kwargs)
 
     def start_requests(self):
         url = 'http://comprasestatales.gub.uy/ocds/rss/{0.year:d}/{0.month:02d}'
-
-        start = date(2017, 11, 1)
-        stop = date.today().replace(day=1)
+        start = self.from_date if self.from_date else datetime.strptime(self.default_from_date, self.date_format)
+        stop = self.until_date if self.until_date else date.today().replace(day=1)
         if self.sample:
             start = stop
 
