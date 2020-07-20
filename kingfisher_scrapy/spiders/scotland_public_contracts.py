fe7ecc0e1860b2f65ffe54b03795bd7493058d96
aguilerapy <48607824+aguilerapy@users.noreply.github.com>
2020-07-20 12:38:26 -0400

    Update kingfisher_scrapy/spiders/scotland_public_contracts.py
    Co-authored-by: Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>

--- kingfisher_scrapy/spiders/scotland_public_contracts.py
+++ kingfisher_scrapy/spiders/scotland_public_contracts.py
@@ -9,7 +9,7 @@ class ScotlandPublicContracts(ScotlandBase):
       sample
         Downloads packages for releases dated one year ago, for each notice type available.
       from_date
-        Download only data from this month onward (MM-YYYY format). Defaults to one year back.
+        Download only data from this month onward (YYYY-MM format). Defaults to one year back.
     """
     name = 'scotland_public_contracts'
     data_type = 'release_package'
