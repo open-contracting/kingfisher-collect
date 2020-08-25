cde40a7651586ca128917f2b69deb29d41df3766
aguilerapy <48607824+aguilerapy@users.noreply.github.com>
2020-08-25 14:42:30 -0400

    Update kingfisher_scrapy/spiders/nigeria_budeshi_records.py
    Co-authored-by: Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>

--- kingfisher_scrapy/spiders/nigeria_budeshi_records.py
+++ kingfisher_scrapy/spiders/nigeria_budeshi_records.py
@@ -7,7 +7,7 @@ class NigeriaBudeshiRecords(NigeriaBudeshiBase):
       https://budeshi.ng/Api
     Spider arguments
       sample
-        Download only the record package with project id = 1.
+        Download only the first record package from https://budeshi.ng/api/project_list.
     """
     name = 'nigeria_budeshi_records'
     data_type = 'record_package'
