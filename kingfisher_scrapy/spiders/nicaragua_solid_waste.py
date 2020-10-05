91b7ff2a38e5303655f50ef100bcb18a609680db
Andres Aguilera <amaguilera96@gmail.com>
2020-10-05 12:36:45 -0300

    Update changes

--- kingfisher_scrapy/spiders/nicaragua_solid_waste.py
+++ kingfisher_scrapy/spiders/nicaragua_solid_waste.py
@@ -17,19 +17,17 @@ class NicaraguaSolidWaste(SimpleSpider):
     name = 'nicaragua_solid_waste'
     data_type = 'release_package'
     default_from_date = '2000-01-01'
-    url = 'http://www.gekoware.com/swmp/api/ocds/'
+    date_required = True
+    url = 'http://www.gekoware.com/swmp/api/ocds/{}/{}'
 
     def start_requests(self):
         if self.sample:
-            # date parameter setting to get a one release from 2013
-            url = self.url + '20130123/20130123'
+            # date parameter setting to get one release from 2013
+            url = self.url.format('20130123', '20130123')
         else:
             if self.from_date and self.until_date:
                 # date parameter obtained
-                url = self.url + self.from_date.strftime("%Y%m%d") + '/' + self.until_date.strftime("%Y%m%d")
-            else:
-                # date parameter setting to get all releases
-                url = self.url + '20000101/20201231'
+                url = self.url.format(self.from_date.strftime("%Y%m%d"), self.until_date.strftime("%Y%m%d"))
 
         # url looks like http://www.gekoware.com/swmp/api/ocds/20190101/20201005
         yield self.build_request(url, formatter=components(-2))
