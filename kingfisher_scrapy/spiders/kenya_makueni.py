a1377edbb8c71fdaccf8501e4cd3f1fae6625814
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-10-16 17:07:06 -0400

    IndexSpider: Tidy documentation and variable names to make code easier to read

--- kingfisher_scrapy/spiders/kenya_makueni.py
+++ kingfisher_scrapy/spiders/kenya_makueni.py
@@ -16,17 +16,17 @@ class KenyaMakueni(IndexSpider):
     """
     name = 'kenya_makueni'
     data_type = 'release_package_list'
-    step = 10
-    additional_params = {'pageSize': step}
+    limit = 10
+    additional_params = {'pageSize': limit}
     yield_list_results = False
     param_page = 'pageNumber'
     formatter = staticmethod(parameters('pageNumber'))
 
-    base_url = 'https://opencontracting.makueni.go.ke/api/ocds/package/all?pageSize={step}&pageNumber={page}'
+    base_url = 'https://opencontracting.makueni.go.ke/api/ocds/package/all?pageSize={limit}&pageNumber={page}'
 
     def start_requests(self):
         if self.sample:
-            url = self.url.format(step=self.step, page=0)
+            url = self.url.format(limit=self.limit, page=0)
             yield self.build_request(url, formatter=parameters('pageNumber'))
         else:
             yield scrapy.Request(
@@ -36,8 +36,7 @@ class KenyaMakueni(IndexSpider):
             )
 
     def range_generator(self, data, response):
-        total = int(response.text)
-        return range(ceil(total / self.step))
+        return range(ceil(int(response.text) / self.limit))
 
     def url_builder(self, params, data, response):
         return self.pages_url_builder(params, data, response)
