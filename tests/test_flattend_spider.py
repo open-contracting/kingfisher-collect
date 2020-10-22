2bb1b785654223f7f5faeed87853b61fc8fee15e
Andres Aguilera <amaguilera96@gmail.com>
2020-10-22 12:47:15 -0300

    Update tests

--- tests/test_flattend_spider.py
+++ tests/test_flattend_spider.py
@@ -2,7 +2,6 @@ import pytest
 from flattentool.input import BadXLSXZipFile
 
 from kingfisher_scrapy.base_spider import FlattenSpider
-from kingfisher_scrapy.exceptions import SpreadsheetExtensionError
 from kingfisher_scrapy.items import File
 from tests import response_fixture, spider_with_crawler
 
@@ -12,10 +11,21 @@ def test_parse():
     spider.data_type = 'release'
 
     generator = spider.parse(response_fixture(url_path='/example/file.csv'))
-    item = next(generator)
+    csv_item = next(generator)
+    json_item = next(generator)
 
-    assert type(item) is File
-    assert item == {
+    assert type(csv_item) is File
+    assert csv_item == {
+        'data': b'{"links": {"next": "http://example.com/next"}}',
+        'data_type': 'release',
+        'encoding': 'utf-8',
+        'file_name': 'file.csv',
+        'post_to_api': False,
+        'url': 'http://example.com/example/file.csv'
+    }
+
+    assert type(json_item) is File
+    assert json_item == {
         'data': '{\n    "releases": []\n}',
         'data_type': 'release',
         'encoding': 'utf-8',
@@ -27,17 +37,38 @@ def test_parse():
 
 def test_xlsx_file_error():
     spider = spider_with_crawler(spider_class=FlattenSpider)
+    spider.data_type = 'release'
 
     generator = spider.parse(response_fixture(url_path='/example/file.xlsx'))
+    item = next(generator)
+
+    assert type(item) is File
+    assert item == {
+        'data': b'{"links": {"next": "http://example.com/next"}}',
+        'data_type': 'release',
+        'encoding': 'utf-8',
+        'file_name': 'file.xlsx',
+        'post_to_api': False,
+        'url': 'http://example.com/example/file.xlsx'
+    }
+
     with pytest.raises(BadXLSXZipFile) as e:
         next(generator)
     assert str(e.value) == "The supplied file has extension .xlsx but isn't an XLSX file."
 
 
-def test_spreadsheet_extension_error():
+def test_extension_error():
     spider = spider_with_crawler(spider_class=FlattenSpider)
+    spider.data_type = 'release'
 
     generator = spider.parse(response_fixture(url_path='/example/file'))
-    with pytest.raises(SpreadsheetExtensionError) as e:
-        next(generator)
-    assert str(e.value) == 'The file has no extension or is not CSV or XLSX.'
+    item = next(generator)
+
+    assert item == {
+        'errors': {
+            'detail': 'The file has no extension or is not CSV or XLSX',
+            'http_code': 400
+        },
+        'file_name': 'file',
+        'url': 'http://example.com/example/file'
+    }
