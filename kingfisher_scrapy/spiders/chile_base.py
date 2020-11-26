5df230f3603819c90445a84ddf5acbf8421605ca
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2020-11-26 14:51:55 -0300

    Update Chile Compra Spider
    - Add new urls
    - Remove year and extend from PeriodicSpider instead
    
    Signed-off-by: Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>

--- kingfisher_scrapy/spiders/chile_base.py
+++ kingfisher_scrapy/spiders/chile_base.py
@@ -1,46 +1,52 @@
 import json
 from datetime import date
 
-from kingfisher_scrapy.base_spider import IndexSpider
-from kingfisher_scrapy.util import components, date_range_by_month, handle_http_error
+from kingfisher_scrapy.base_spider import IndexSpider, PeriodicSpider
+from kingfisher_scrapy.exceptions import SpiderArgumentError
+from kingfisher_scrapy.util import components, handle_http_error
 
 
-class ChileCompraBaseSpider(IndexSpider):
+class ChileCompraBaseSpider(IndexSpider, PeriodicSpider):
     custom_settings = {
         'DOWNLOAD_FAIL_ON_DATALOSS': False,
     }
 
+    pattern = 'http://api.mercadopublico.cl/APISOCDS/OCDS/{0}/{1.year:d}/{1.month:02d}/{2}/{3}'
+    available_systems = {
+        'convenio': 'listaOCDSAgnoMesConvenio',
+        'licitacion': 'listaOCDSAgnoMes',
+        'trato-directo': 'listaOCDSAgnoMesTratoDirecto'
+    }
+    system = None
+
+    # from PeriodicSpider
+    date_format = 'year-month'
+    default_from_date = '2008-01'
+    start_requests_callback = 'build_periodic_requests'
+
+    # from IndexSpider
     limit = 100
-    base_list_url = 'https://apis.mercadopublico.cl/OCDS/data/listaA%C3%B1oMes/{0.year:d}/{0.month:02d}/{1}/{2}'
     formatter = staticmethod(components(-4, -1))
     count_pointer = '/pagination/total'
     yield_list_results = False
 
-    def start_requests(self):
-        today = date.today()
-        if hasattr(self, 'year'):
-            year = int(self.year)
-            start = date(year, 1, 1)
-            stop = date(year, 12, 1)
-            if year == today.year:
-                stop = stop.replace(month=today.month)
-        else:
-            start = date(2008, 1, 1)
-            stop = today
+    @classmethod
+    def from_crawler(cls, crawler, system=None, *args, **kwargs):
+        spider = super().from_crawler(crawler, system=system, *args, **kwargs)
+        if system and spider.system not in spider.available_systems.keys():
+            raise SpiderArgumentError(f'spider argument `system`: {spider.system!r} not recognized')
+        return spider
 
-        for d in date_range_by_month(start, stop):
-            yield self.build_request(
-                self.base_list_url.format(d, 0, self.limit),
-                formatter=components(-4, -1),
-                meta={
-                    'year': d.year,
-                    'month': d.month,
-                },
-                callback=self.parse_list
-            )
+    def build_urls(self, date):
+        for system in self.available_systems.keys():
+            if self.system and system != self.system:
+                continue
+            yield self.pattern.format(self.available_systems[system], date, 0, self.limit)
 
-    @handle_http_error
-    def parse_list(self, response, **kwargs):
+    def get_formatter(self):
+        return components(-4, -1)
+
+    def parse(self, response):
         data = json.loads(response.text)
         # Some files contain invalid packages, e.g.:
         # {
@@ -51,12 +57,17 @@ class ChileCompraBaseSpider(IndexSpider):
             data['http_code'] = data['status']
             yield self.build_file_error_from_response(response, errors=data)
             return
+        yield from super().parse(response)
 
-        kwargs['callback'] = self.parse_items
-        yield from super().parse_list(response, **kwargs)
-        yield from self.parse_items(response)
+    @handle_http_error
+    # from PeriodicSpider
+    def build_periodic_requests(self, response, **kwargs):
+        yield from self.parse_page(response)
+        kwargs['callback'] = self.parse_page
+        yield from self.parse_list(response, **kwargs)
 
-    def parse_items(self, response):
+    @handle_http_error
+    def parse_page(self, response, **kwargs):
         data = json.loads(response.text)
         for item in data['data']:
             # An item looks like:
@@ -69,8 +80,10 @@ class ChileCompraBaseSpider(IndexSpider):
             # }
             yield from self.handle_item(item)
 
+    # from IndexSpider
     def url_builder(self, value, data, response):
-        year = response.request.meta['year']
-        month = response.request.meta['month']
+        system = components(-5, -4)(response.request.url)
+        year = int(components(-4, -3)(response.request.url))
+        month = int(components(-3, -2)(response.request.url).lstrip('0'))
 
-        return self.base_list_url.format(date(year, month, 1), value, self.limit)
+        return self.pattern.format(system, date(year, month, 1), value, self.limit)
--- kingfisher_scrapy/spiders/chile_compra_releases.py
+++ kingfisher_scrapy/spiders/chile_compra_releases.py
@@ -6,6 +6,21 @@ class ChileCompraReleases(ChileCompraBaseSpider):
     """
     Domain
       ChileCompra
+    Spider arguments
+      from_date
+        Download only releases from this date onward (YYYY-MM format).
+        If from_date is not provided defaults to 2008-01
+      until_date
+        Download only releases until this date (YYYY-MM format).
+        If from_date is not provided defaults to current year and month.
+      system
+        Filter by system, if not set gets all:
+          convenio
+            Framework agreements only
+          trato-directo
+            Direct tenders only
+          licitacion
+            Traditional tenders
     Swagger API documentation
       https://desarrolladores.mercadopublico.cl/docs/services/5a9ed02f718ed712f4403e75/operations/data-listaa-omes-agno-mes
     """
