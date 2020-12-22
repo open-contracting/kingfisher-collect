a067453792cd6e3cbf9d14fb18aeaef031eaaeac
aguilerapy <amaguilera96@gmail.com>
2020-12-22 17:15:35 -0300

    Update Indonesia scraper

--- kingfisher_scrapy/spiders/indonesia_bandung.py
+++ kingfisher_scrapy/spiders/indonesia_bandung.py
@@ -1,14 +1,20 @@
 import json
-from datetime import date
 
-from kingfisher_scrapy.base_spider import BaseSpider
-from kingfisher_scrapy.util import components, date_range_by_year, handle_http_error, join, parameters
+from kingfisher_scrapy.base_spider import PeriodicSpider
+from kingfisher_scrapy.util import components, handle_http_error, join, parameters
 
 
-class IndonesiaBandung(BaseSpider):
+class IndonesiaBandung(PeriodicSpider):
     """
     Domain
       Bandung Integrated Resource Management System (BIRMS)
+    Spider arguments
+      from_date
+        Download only releases from this date onward (YYYY format).
+        If ``from_date`` is not provided defaults to 2013.
+      until_date
+        Download only releases until this date (YYYY format).
+        If ``from_date`` is not provided defaults to current year.
     API endpoints
       Get contract list by year
         Link
@@ -32,14 +38,11 @@ class IndonesiaBandung(BaseSpider):
     name = 'indonesia_bandung'
     data_type = 'release'
 
-    def start_requests(self):
-        pattern = 'https://birms.bandung.go.id/api/packages/year/{}'
-
-        start = 2013
-        stop = date.today().year
-
-        for year in date_range_by_year(start, stop):
-            yield self.build_request(pattern.format(year), formatter=components(-1), callback=self.parse_list)
+    # PeriodicSpider variables
+    date_format = 'year'
+    default_from_date = '2013'
+    pattern = 'https://birms.bandung.go.id/api/packages/year/{}'
+    start_requests_callback = 'parse_list'
 
     @handle_http_error
     def parse_list(self, response):
@@ -47,11 +50,11 @@ class IndonesiaBandung(BaseSpider):
         for item in data['data']:
             url = item['uri']
             if url:
-                yield self.build_request(url, formatter=components(-1))
+                yield self.build_request(url, self.get_formatter())
         else:
             next_page_url = data.get('next_page_url')
             if next_page_url:
-                yield self.build_request(next_page_url, formatter=join(components(-1), parameters('page')),
+                yield self.build_request(next_page_url, formatter=join(self.get_formatter(), parameters('page')),
                                          callback=self.parse_list)
 
     @handle_http_error
@@ -60,3 +63,6 @@ class IndonesiaBandung(BaseSpider):
         if len(data) == 0:
             return
         yield self.build_file_from_response(response, data_type=self.data_type)
+
+    def get_formatter(self):
+        return components(-1)
