455d8cb23ded924a76c45d70ec7f3d2411b820a7
aguilerapy <amaguilera96@gmail.com>
2021-01-07 15:35:00 -0300

    costa_rica_poder_judicial_records: add date filters

--- kingfisher_scrapy/spiders/costa_rica_poder_judicial_records.py
+++ kingfisher_scrapy/spiders/costa_rica_poder_judicial_records.py
@@ -1,13 +1,16 @@
-import scrapy
-
-from kingfisher_scrapy.base_spider import SimpleSpider
-from kingfisher_scrapy.util import components, handle_http_error
+from kingfisher_scrapy.base_spider import PeriodicSpider
+from kingfisher_scrapy.util import components
 
 
-class CostaRicaPoderJudicialRecords(SimpleSpider):
+class CostaRicaPoderJudicialRecords(PeriodicSpider):
     """
     Domain
       Poder Judicial de Costa Rica
+    Spider arguments
+      from_date
+        Download only data from this month onward (YYYY format). Defaults to '2016'.
+      until_date
+        Download only data until this month (YYYY format). Defaults to the current year.
     Bulk download documentation
       http://datosabiertospj.eastus.cloudapp.azure.com/dataset/estandar-de-datos-de-contrataciones-abiertas-ocds
     """
@@ -15,14 +18,10 @@ class CostaRicaPoderJudicialRecords(SimpleSpider):
     name = 'costa_rica_poder_judicial_records'
     data_type = 'record_package'
 
-    def start_requests(self):
-        url = 'http://datosabiertospj.eastus.cloudapp.azure.com/api/3/action/package_show?id=estandar-de-datos-de' \
-              '-contrataciones-abiertas-ocds'
-        yield scrapy.Request(url, meta={'file_name': 'list.json'}, callback=self.parse_list)
+    # PeriodicSpider variables
+    date_format = 'year'
+    default_from_date = '2016'
+    pattern = 'https://pjcrdatosabiertos.blob.core.windows.net/datosabiertos/OpenContracting/{}.json'
 
-    @handle_http_error
-    def parse_list(self, response):
-        data = response.json()
-        for resource in data['result']['resources']:
-            if resource['format'].upper() == 'JSON':
-                yield self.build_request(resource['url'], formatter=components(-1))
+    def get_formatter(self):
+        return components(-1)
