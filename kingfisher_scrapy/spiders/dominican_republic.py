809d2d082aab71c5ae9f3ecfa22af6e302cfc33b
aguilerapy <amaguilera96@gmail.com>
2021-01-14 16:19:17 -0300

    dominican_republic: add date filters

--- kingfisher_scrapy/spiders/dominican_republic.py
+++ kingfisher_scrapy/spiders/dominican_republic.py
@@ -8,10 +8,18 @@ class DominicanRepublic(CompressedFileSpider):
     """
     Domain
       Dirección General de Contrataciones Públicas (DGCP)
+    Spider arguments
+      from_date
+        Download only data from this year onward (YYYY format).
+        If ``until_date`` is provided, defaults to '2018'.
+      until_date
+        Download only data until this date (YYYY format).
+        If ``from_date`` is provided, defaults to current year.
     Bulk download documentation
       https://www.dgcp.gob.do/estandar-mundial-ocds/
     """
     name = 'dominican_republic'
+    date_format = 'year'
     data_type = 'release_package'
     compressed_file_format = 'release_package'
     archive_format = 'rar'
@@ -25,9 +33,12 @@ class DominicanRepublic(CompressedFileSpider):
 
     @handle_http_error
     def parse_list(self, response):
-        urls = response.css('.fileLink::attr(href)').getall()
+        urls = response.css('.download::attr(href)').getall()
         json_urls = list(filter(lambda x: '/JSON_DGCP_' in x, urls))
 
         for url in json_urls:
             if '/JSON_DGCP_' in url:
+                if self.from_date and self.until_date:
+                    if not (self.from_date.year <= int(url[-8:-4]) <= self.until_date.year):
+                        continue
                 yield self.build_request(url, formatter=components(-1))
