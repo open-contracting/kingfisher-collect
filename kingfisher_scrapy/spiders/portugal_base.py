3494af62a23f5aed76234e2e33f3444eeb6812c0
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2021-01-15 19:07:26 -0300

    portugal: add autothrotle and sleep if error
    Signed-off-by: Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>

--- kingfisher_scrapy/spiders/portugal_base.py
+++ kingfisher_scrapy/spiders/portugal_base.py
@@ -1,3 +1,5 @@
+import time
+
 import scrapy
 
 from kingfisher_scrapy.base_spider import LinksSpider
@@ -11,7 +13,10 @@ class PortugalBase(LinksSpider):
     download_delay = 1
     # The API returns 503 error sometimes
     custom_settings = {
-        'RETRY_TIMES': 10
+        'RETRY_TIMES': 10,
+        'AUTOTHROTTLE_ENABLED': True,
+        'AUTOTHROTTLE_TARGET_CONCURRENCY': 0.5,
+        'HTTPERROR_ALLOW_ALL': True
     }
 
     def start_requests(self):
@@ -19,3 +24,13 @@ class PortugalBase(LinksSpider):
         if self.from_date and self.until_date:
             url = f'{url}&contractStartDate={self.from_date}&contractEndDate={self.until_date}'
         yield scrapy.Request(url, meta={'file_name': 'offset-1.json'})
+
+    def parse(self, response):
+        # after a undefined number of requests, the API returns 5XX errors. After waiting a few seconds the URL
+        # works again
+        if not self.is_http_success(response):
+            self.logger.info(f'Response status {response.status} waiting 120 seconds before continue')
+            time.sleep(120)
+            yield scrapy.Request(response.request.url, dont_filter=True, meta=response.request.meta)
+        else:
+            yield from super().parse(response)
