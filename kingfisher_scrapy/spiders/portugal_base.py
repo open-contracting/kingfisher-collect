a4f2bbaacf73315a41651cec8a3eec8285e18294
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2021-01-19 08:06:21 -0300

    portugal: increment number of retries and wating time
    Signed-off-by: Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>

--- kingfisher_scrapy/spiders/portugal_base.py
+++ kingfisher_scrapy/spiders/portugal_base.py
@@ -20,6 +20,8 @@ class PortugalBase(LinksSpider):
     }
 
     number_of_retries = 0
+    max_number_of_retries = 15
+    wait_time = 180
 
     def start_requests(self):
         url = self.url
@@ -28,19 +30,22 @@ class PortugalBase(LinksSpider):
         yield scrapy.Request(url, meta={'file_name': 'offset-1.json'})
 
     def parse(self, response):
-        # after a undefined number of requests, the API returns 5XX errors. After waiting a few seconds the URL
-        # works again
+        # after a undefined number of requests (around 36100?), the API returns 5XX errors.
+        # After waiting a few seconds the URL works again
         if not self.is_http_success(response):
-            if self.number_of_retries < 5:
-                self.logger.info(f'Response status {response.status} waiting 180 seconds before continue, '
-                                 f'attempt {self.number_of_retries}')
+            if self.number_of_retries < self.max_number_of_retries:
+                self.logger.info(f'Response status {response.status} waiting {self.wait_time} '
+                                 f'seconds before continue, attempt {self.number_of_retries}')
                 yield self.build_file_error_from_response(response)
-                time.sleep(180)
+                time.sleep(self.wait_time)
                 self.number_of_retries = self.number_of_retries + 1
+                # if it fails again for the same request, we wait more
+                self.wait_time = self.wait_time * self.number_of_retries
                 yield scrapy.Request(response.request.url, dont_filter=True, meta=response.request.meta)
             else:
                 self.logger.info(f'Response status {response.status}, maximum attempts reached, giving up')
                 yield self.build_file_error_from_response(response)
         else:
             self.number_of_retries = 0
+            self.wait_time = 180
             yield from super().parse(response)
