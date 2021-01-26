136812c53faec00632cb8ba2062b7e67a8158f96
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2021-01-26 13:11:33 -0300

    Update portugal to use DelayedRequestsMiddleware and retries in Request meta
    Signed-off-by: Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>

--- kingfisher_scrapy/spiders/portugal_base.py
+++ kingfisher_scrapy/spiders/portugal_base.py
@@ -1,7 +1,4 @@
 import scrapy
-from scrapy import signals
-from scrapy.exceptions import DontCloseSpider
-from twisted.internet import defer, reactor
 
 from kingfisher_scrapy.base_spider import LinksSpider
 from kingfisher_scrapy.util import parameters
@@ -14,20 +11,14 @@ class PortugalBase(LinksSpider):
     download_delay = 1
 
     # We will wait 1, 2, 4, 8, 16 minutes (31 minutes total).
-    retries = 0
-    max_retries = 5
-    wait_time = initial_wait_time = 60
+    max_retries = 6
+    initial_wait_time = 30
 
-    # https://stackoverflow.com/questions/46698333/deferred-requests-in-scrapy
-    @classmethod
-    def from_crawler(cls, crawler, **kwargs):
-        spider = super().from_crawler(crawler, **kwargs)
-        crawler.signals.connect(spider.spider_idle, signal=signals.spider_idle)
-        return spider
-
-    @classmethod
-    def spider_idle(cls, spider):
-        raise DontCloseSpider()
+    custom_settings = {
+        'DOWNLOADER_MIDDLEWARES': {
+            'kingfisher_scrapy.middlewares.DelayedRequestsMiddleware': 543,
+        }
+    }
 
     def start_requests(self):
         url = self.url
@@ -37,28 +28,26 @@ class PortugalBase(LinksSpider):
         yield scrapy.Request(url, meta={'file_name': 'offset-1.json'})
 
     def parse(self, response):
+
+        retries = response.request.meta.get('retries', 0)
+        wait_time = response.request.meta.get('delay_request', self.initial_wait_time)
+
         # Every ~36,000 requests, the API returns HTTP errors. After a few minutes, it starts working again.
         # https://github.com/open-contracting/kingfisher-collect/issues/545#issuecomment-762768460
         if self.is_http_success(response):
             yield from super().parse(response)
-        elif self.retries < self.max_retries:
+        elif retries < self.max_retries:
+            response.request.meta['retries'] += 1
+            response.request.meta['delay_request'] *= 2
             request = scrapy.Request(response.request.url, meta=response.request.meta, dont_filter=True)
 
             self.logger.debug('Retrying %(request)s in %(wait_time)ds (failed %(retries)d times): HTTP %(status)d',
-                              {'request': response.request, 'retries': self.retries, 'status': response.status,
-                               'wait_time': self.wait_time}, extra={'spider': self})
-            deferred = defer.Deferred()
-            reactor.callLater(self.wait_time, self.crawler.engine.schedule, request, spider=self)
+                              {'request': response.request, 'retries': retries, 'status': response.status,
+                               'wait_time': wait_time}, extra={'spider': self})
 
-            self.retries += 1
-            self.wait_time *= 2
-
-            return deferred
+            yield request
         else:
             self.logger.error('Gave up retrying %(request)s (failed %(retries)d times): HTTP %(status)d',
-                              {'request': response.request, 'retries': self.retries, 'status': response.status},
+                              {'request': response.request, 'retries': retries, 'status': response.status},
                               extra={'spider': self})
             yield self.build_file_error_from_response(response)
-
-        self.retries = 0
-        self.wait_time = self.initial_wait_time
