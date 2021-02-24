185bdc815bc6fcacf7047f06814252565e315a8b
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-02-24 00:20:34 -0500

    digiwhist_armenia, digiwhist_serbia: Remove (no longer available)

--- kingfisher_scrapy/spiders/digiwhist_serbia.py
+++
@@ -1,6 +0,0 @@
-from kingfisher_scrapy.spiders.digiwhist_base import DigiwhistBase
-
-
-class DigiwhistSerbia(DigiwhistBase):
-    name = 'digiwhist_serbia'
-    start_urls = ['https://opentender.eu/data/files/RS_ocds_data.json.tar.gz']
