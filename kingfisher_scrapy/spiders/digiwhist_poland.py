0b0f39769a099247a23049bd8bfa82f712c4a36c
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2021-02-25 19:53:29 -0300

    digiwhist_poland: increase download_timeout
    Signed-off-by: Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>

--- kingfisher_scrapy/spiders/digiwhist_poland.py
+++ kingfisher_scrapy/spiders/digiwhist_poland.py
@@ -4,3 +4,5 @@ from kingfisher_scrapy.spiders.digiwhist_base import DigiwhistBase
 class DigiwhistPoland(DigiwhistBase):
     name = 'digiwhist_poland'
     start_urls = ['https://opentender.eu/data/files/PL_ocds_data.json.tar.gz']
+    # 20 minutes, 2.7GB file
+    download_timeout = 1200
