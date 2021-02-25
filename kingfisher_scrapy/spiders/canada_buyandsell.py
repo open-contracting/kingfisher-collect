fb676ffe1b531bcf68fa670536d41d8f7d756273
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2021-02-25 20:11:39 -0300

    canada_buyandsell: correct links
    Signed-off-by: Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>

--- kingfisher_scrapy/spiders/canada_buyandsell.py
+++ kingfisher_scrapy/spiders/canada_buyandsell.py
@@ -21,10 +21,10 @@ class CanadaBuyAndSell(SimpleSpider):
 
     def start_requests(self):
         urls = [
-            'https://buyandsell.gc.ca/cds/public/ocds/tpsgc-pwgsc_ocds_EF-FY-16-17.json',
             'https://buyandsell.gc.ca/cds/public/ocds/tpsgc-pwgsc_ocds_EF-FY-15-16.json',
             'https://buyandsell.gc.ca/cds/public/ocds/tpsgc-pwgsc_ocds_EF-FY-14-15.json',
             'https://buyandsell.gc.ca/cds/public/ocds/tpsgc-pwgsc_ocds_EF-FY-13-14.json',
+            'https://buyandsell.gc.ca/cds/public/ocds/tpsgc-pwgsc_ocds_EF-FY-12-13.json',
         ]
 
         for url in urls:
