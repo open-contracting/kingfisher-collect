804ea4a9b24606b32a09669ab60bf758631b742b
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2021-02-25 11:12:44 -0300

    test_kingfisher_pluck_extension add tests for bytes_received
    Signed-off-by: Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>

--- tests/extensions/test_kingfisher_pluck.py
+++ tests/extensions/test_kingfisher_pluck.py
@@ -1,6 +1,11 @@
+
 import os
 from glob import glob
 from tempfile import TemporaryDirectory
+
+import pytest
+from scrapy import Request
+from scrapy.exceptions import StopDownload
 
 from kingfisher_scrapy.extensions import KingfisherPluck
 from kingfisher_scrapy.items import PluckedItem
@@ -67,3 +72,29 @@ def test_spider_closed_without_items():
 
         with open(os.path.join(tmpdirname, 'pluck-release-date.csv')) as f:
             assert 'closed: itemcount,test\n' == f.read()
+
+
+def test_bytes_received_stop_download():
+    with TemporaryDirectory() as tmpdirname:
+        spider = spider_with_crawler(settings={'KINGFISHER_PLUCK_PATH': tmpdirname,
+                                               'KINGFISHER_PLUCK_MAX_BYTES': 1}, release_pointer='/date')
+        extension = KingfisherPluck.from_crawler(spider.crawler)
+        request = Request('http://example.com', meta={'file_name': 'test.json'})
+
+        with pytest.raises(StopDownload):
+            extension.bytes_received(data={'test': 'test'}, spider=spider, request=request)
+
+        assert extension.max_bytes == 1
+
+
+def test_bytes_received_dont_stop_download():
+    with TemporaryDirectory() as tmpdirname:
+        spider = spider_with_crawler(settings={'KINGFISHER_PLUCK_PATH': tmpdirname,
+                                               'KINGFISHER_PLUCK_MAX_BYTES': 10}, release_pointer='/date')
+        extension = KingfisherPluck.from_crawler(spider.crawler)
+        request = Request('http://example.com', meta={'file_name': 'test.json'})
+
+        extension.bytes_received(data={'test': 'test'}, spider=spider, request=request)
+        assert extension.bytes_received_per_spider[spider.name] == 1
+
+        assert extension.max_bytes == 10
