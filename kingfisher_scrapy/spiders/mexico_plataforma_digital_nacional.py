c2a805dfc2b995828ad75fd51ba79a2823a38822
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2021-03-02 13:48:55 -0300

    spider: add Mexico Plataforma Digital Nacional
    Signed-off-by: Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>

---
+++ kingfisher_scrapy/spiders/mexico_plataforma_digital_nacional.py
@@ -0,0 +1,31 @@
+import scrapy
+
+from kingfisher_scrapy.base_spider import CompressedFileSpider
+from kingfisher_scrapy.util import components
+
+
+class MexicoPlataformaDigitalNacional(CompressedFileSpider):
+
+    """
+    Domain
+      Plataforma Digital Nacional
+    Bulk download documentation
+      https://plataformadigitalnacional.org/contrataciones
+    """
+    name = 'mexico_plataforma_digital_nacional'
+    data_type = 'release'
+    root_path = 'item'
+    google_api_key = ''
+
+    def start_requests(self):
+        yield scrapy.Request(
+            'https://drive.google.com/u/0/uc?id=1XOYDLVv-RqcMs8_hzkZ0fjC9FYh2psFw&export=download',
+            meta={'file_name': 'list.html'},
+            callback=self.parse_list,
+        )
+
+    def parse_list(self, response):
+        url = response.xpath('//a[@id="uc-download-link"]/@href').get()
+        yield self.build_request(url=f'https://drive.google.com/u/0{url}&key={self.google_api_key}',
+                                 formatter=components(-1),
+                                 meta={'file_name': 'contrataciones.zip'})
