c60e0c7a132f16d05377b7c811678c19686d28af
aguilerapy <amaguilera96@gmail.com>
2021-03-10 19:07:52 -0300

    Add 'mexico_apf_bulk'

--- kingfisher_scrapy/spiders/mexico_apf_bulk.py
+++ kingfisher_scrapy/spiders/mexico_apf_bulk.py
@@ -1,30 +1,23 @@
-import scrapy
-
-from kingfisher_scrapy.base_spider import IndexSpider
-from kingfisher_scrapy.util import parameters
+from kingfisher_scrapy.base_spider import CompressedFileSpider
+from kingfisher_scrapy.util import components
 
 
-class MexicoAPF(IndexSpider):
+class MexicoAPFBulk(CompressedFileSpider):
     """
     Domain
-      Administración Pública Federal (APF)
+      Administración Pública Federal (APF) - Secretaria de la Función Pública (SFP) - Secretaría de Hacienda y Crédito
+      Público (SHCP)
     Bulk download documentation
       https://datos.gob.mx/busca/dataset/concentrado-de-contrataciones-abiertas-de-la-apf
     """
-    name = 'mexico_apf_api'
+    name = 'mexico_apf_bulk'
 
     # BaseSpider
-    root_path = 'results.item'
+    root_path = 'item'
 
-    # SimpleSpider
-    data_type = 'record_package'
-
-    # IndexSpider
-    count_pointer = '/pagination/total'
-    limit = '/pagination/pageSize'
-    use_page = True
-    formatter = staticmethod(parameters('page'))
+    # CompressedFileSpider
+    data_type = 'release'
 
     def start_requests(self):
-        url = 'https://api.datos.gob.mx/v1/contratacionesabiertas'
-        yield scrapy.Request(url, meta={'file_name': 'page-1.json'}, callback=self.parse_list)
+        url = 'https://compranetinfo.hacienda.gob.mx/dabiertos/contrataciones_arr.json.zip'
+        yield self.build_request(url, formatter=components(-1))
