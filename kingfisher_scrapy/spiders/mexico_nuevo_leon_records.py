39564e160d0c1d195408400d80228064f661ddf5
aguilerapy <amaguilera96@gmail.com>
2021-03-10 20:40:26 -0300

    Update URL

--- kingfisher_scrapy/spiders/mexico_nuevo_leon_records.py
+++ kingfisher_scrapy/spiders/mexico_nuevo_leon_records.py
@@ -1,20 +1,31 @@
-from kingfisher_scrapy.spiders.mexico_nuevo_leon_base import MexicoNuevoLeonBase
+import scrapy
+
+from kingfisher_scrapy.base_spider import SimpleSpider
+from kingfisher_scrapy.util import components, handle_http_error
 
 
-class MexicoNuevoLeonRecords(MexicoNuevoLeonBase):
+class MexicoNuevoLeonRecords(SimpleSpider):
     """
     Domain
       Secretaría de Infraestructura del Gobierno del Estado de Nuevo León
     Bulk download documentation
-      http://si.nl.gob.mx/transparencia/acerca-del-proyecto
+      http://si.nl.gob.mx/transparencia/publicaciones
     """
     name = 'mexico_nuevo_leon_records'
-
-    # BaseSpider
-    skip_pluck = 'Already covered (see code for details)'  # mexico_nuevo_leon_releases
 
     # SimpleSpider
     data_type = 'record_package'
 
-    # CompressedFileSpider
-    file_name_must_contain = 'RecordPackage'
+    def start_requests(self):
+        yield scrapy.Request(
+            'http://si.nl.gob.mx/siasi_ws/api/ocds/DescargarRecordPackage?usuario=undefined&servidor=undefined',
+            meta={'file_name': 'list.json'},
+            callback=self.parse_list
+        )
+
+    @handle_http_error
+    def parse_list(self, response):
+        content = response.json()
+        url = content['urlWebOcdspackage']
+        if url:
+            yield self.build_request(url, formatter=components(-1))
