6b919c4a372aef0eecb159937c1a22b17031bfb9
alecbass <alec.bassingthwaighte@gmail.com>
2021-03-16 12:56:24 +1000

    [668-australia-date-args] Use from_date and until_date arguments in the Australia spider.

--- kingfisher_scrapy/spiders/australia.py
+++ kingfisher_scrapy/spiders/australia.py
@@ -15,15 +15,24 @@ class Australia(LinksSpider):
     Swagger API documentation
       https://app.swaggerhub.com/apis/austender/ocds-api/1.1
     """
-    name = 'australia'
+
+    name = "australia"
+
+    default_from_date = "2004-01-01"
 
     # SimpleSpider
-    data_type = 'release_package'
+    data_type = "release_package"
 
     # LinksSpider
-    next_page_formatter = staticmethod(parameters('cursor'))
+    next_page_formatter = staticmethod(parameters("cursor"))
 
     def start_requests(self):
-        url = f'https://api.tenders.gov.au/ocds/findByDates/contractPublished/' \
-              f'2004-01-01T00:00:00Z/{date.today().year}-12-31T23:59:59Z'
-        yield scrapy.Request(url, meta={'file_name': 'start.json'})
+        from_date = self.from_date.strftime("%Y-%m-%d") if self.from_date is not None else self.default_from_date
+
+        until_date = self.until_date.strftime("%Y-%m-%d") if self.until_date is not None else f"{date.today().year}-12-31"
+
+        url = (
+            f"https://api.tenders.gov.au/ocds/findByDates/contractPublished/"
+            f"{from_date}T00:00:00Z/{until_date}T23:59:59Z"
+        )
+        yield scrapy.Request(url, meta={"file_name": "start.json"})
