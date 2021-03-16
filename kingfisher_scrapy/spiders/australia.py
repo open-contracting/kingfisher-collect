3a0e2d27e60b41ee7ce6d98c18ddfd598f3313f2
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2021-03-16 10:57:54 -0300

    australia: add from and until date parameters documentation
    Signed-off-by: Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>

--- kingfisher_scrapy/spiders/australia.py
+++ kingfisher_scrapy/spiders/australia.py
@@ -8,6 +8,11 @@ class Australia(LinksSpider):
     """
     Domain
       AusTender
+    Spider arguments
+      from_date
+        Download only contracts from this time onward (YYYY-MM-DDThh:mm:ss format). Defaults to '2004-01-01T00:00:00'.
+      until_date
+        Download only contracts until this time (YYYY-MM-DDThh:mm:ss format). Defaults to now.
     API documentation
       https://github.com/austender/austender-ocds-api
     Swagger API documentation
