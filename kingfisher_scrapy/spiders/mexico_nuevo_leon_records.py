6ff44f7e09c6d145f556546d7e93eada7c05e776
aguilerapy <amaguilera96@gmail.com>
2021-04-19 10:34:42 -0400

    Remove callback function

--- kingfisher_scrapy/spiders/mexico_nuevo_leon_records.py
+++ kingfisher_scrapy/spiders/mexico_nuevo_leon_records.py
@@ -1,7 +1,6 @@
 import scrapy
 
 from kingfisher_scrapy.base_spider import SimpleSpider
-from kingfisher_scrapy.util import components, handle_http_error
 
 
 class MexicoNuevoLeonRecords(SimpleSpider):
@@ -19,13 +18,5 @@ class MexicoNuevoLeonRecords(SimpleSpider):
     def start_requests(self):
         yield scrapy.Request(
             'http://si.nl.gob.mx/siasi_ws/api/ocds/DescargarRecordPackage?usuario=undefined&servidor=undefined',
-            meta={'file_name': 'list.json'},
-            callback=self.parse_list
+            meta={'file_name': 'records.json'}
         )
-
-    @handle_http_error
-    def parse_list(self, response):
-        content = response.json()
-        url = content['urlWebOcdspackage']
-        if url:
-            yield self.build_request(url, formatter=components(-1))
