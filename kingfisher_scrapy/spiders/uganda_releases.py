89b3f504598898459a414f1ba3d2d35c19669639
aguilerapy <amaguilera96@gmail.com>
2021-05-05 17:41:53 -0400

    Update changes from review

--- kingfisher_scrapy/spiders/uganda_releases.py
+++ kingfisher_scrapy/spiders/uganda_releases.py
@@ -10,22 +10,24 @@ class Uganda(IndexSpider):
       Government Procurement Portal (GPP) of Public Procurement and Disposal of Public Assets Authority (PPDA)
     Spider arguments
       from_date
-        Download only data from this year onward (YYYY format). Defaults to '2017'.
+        Download only data from this year onward (YYYY format).
+        If ``until_date`` is provided, defaults to '2017'.
         The year refers to the start of the fiscal year range, e.g. if from_date='2017' then fiscal_year='2017-2018'
       until_date
-        Download only data until this year (YYYY format). Defaults to the current year.
+        Download only data until this year (YYYY format).
+        If ``from_date`` is provided, defaults to the current year.
         The year refers to the start of the fiscal year range, e.g. if until_date='2017' then fiscal_year='2017-2018'
     API documentation
       https://docs.google.com/spreadsheets/d/10tVioy-VOQa1FwWoRl5e1pMbGpiymA0iycNcoDFkvks/edit#gid=365266172
     """
     name = 'uganda_releases'
     download_delay = 30  # to avoid API 429 error "too many request"
-    date_format = 'year'
     custom_settings = {
         'CONCURRENT_REQUESTS': 1,
     }
 
     # BaseSpider
+    date_format = 'year'
     default_from_date = '2017'
 
     # SimpleSpider
@@ -54,7 +56,7 @@ class Uganda(IndexSpider):
             for plans in pdes['procurement_plans']:
                 for tag in ('planning', 'tender', 'award', 'contract'):
                     if self.from_date and self.until_date:
-                        start_year = int(plans['financial_year'].split("-")[0])
+                        start_year = int(plans['financial_year'].split('-')[0])
                         if not (self.from_date.year <= start_year <= self.until_date.year):
                             continue
                     yield self.build_request(
