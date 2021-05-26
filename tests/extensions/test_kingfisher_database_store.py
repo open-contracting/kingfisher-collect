605b127019e6c2744ab76db00e12c5c5895ed665
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2021-05-26 11:57:54 -0400

    test: remove format_from_date tests
    Signed-off-by: Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>

--- tests/extensions/test_kingfisher_database_store.py
+++ tests/extensions/test_kingfisher_database_store.py
@@ -18,16 +18,3 @@ def test_from_crawler_missing_arguments():
         DatabaseStore.from_crawler(spider.crawler)
 
     assert str(excinfo.value) == 'FILES_STORE is not set.'
-
-
-@pytest.mark.parametrize('date_format,expected', [
-    ('%Y-%m-%dT%H:%M:%S', '2020-01-01T00:00:00'),
-    ('%Y-%m-%d', '2020-01-01'),
-    ('%Y-%m', '2020-01'),
-    ('%Y', '2020'),
-])
-def test_format_from_date(date_format, expected):
-    spider = spider_with_crawler(crawl_time='2021-05-25T00:00:00', settings={'DATABASE_URL': 'test',
-                                                                             'FILES_STORE': 'test'})
-    extension = DatabaseStore.from_crawler(spider.crawler)
-    assert extension.format_from_date('2020-01-01T00:00:00', date_format, spider.VALID_DATE_FORMATS) == expected
