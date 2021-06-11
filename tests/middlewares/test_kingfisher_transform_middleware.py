72e448e32effa2d9118684d4f17870cba3aa38bd
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2021-06-11 19:33:58 -0400

    isort
    Signed-off-by: Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>

--- tests/middlewares/test_kingfisher_transform_middleware.py
+++ tests/middlewares/test_kingfisher_transform_middleware.py
@@ -6,8 +6,8 @@ import pytest
 
 from kingfisher_scrapy.base_spider import CompressedFileSpider, SimpleSpider
 from kingfisher_scrapy.items import File, FileError, FileItem
-from kingfisher_scrapy.middlewares import (AddPackageMiddleware, LineDelimitedMiddleware, ReadDataMiddleware,
-                                           ResizePackageMiddleware, RootPathMiddleware, ConcatenatedJSONMiddleware)
+from kingfisher_scrapy.middlewares import (AddPackageMiddleware, ConcatenatedJSONMiddleware, LineDelimitedMiddleware,
+                                           ReadDataMiddleware, ResizePackageMiddleware, RootPathMiddleware)
 from tests import response_fixture, spider_with_crawler
 
 
