ea7a9b111f3662bc43352d4c772cccbf2befc8ac
cecicasco <cecilia.casco7@gmail.com>
2021-06-18 09:33:48 -0400

    add mexico idaip spider

---
+++ kingfisher_scrapy/spiders/mexico_idaip.py
@@ -0,0 +1,32 @@
+from kingfisher_scrapy.base_spider import PeriodicSpider
+from kingfisher_scrapy.util import components
+
+
+class MexicoIdaip(PeriodicSpider):
+    """
+    Domain
+      Instituto Duranguense de Acceso a la Información y de Protección de Datos Personales (IDAIP)
+    Spider arguments
+      from_date
+        Download only data from this year onward (YYYY format). Defaults to '2020'.
+      until_date
+        Download only data until this year (YYYY format). Defaults to '2020'.
+    API documentation
+      http://contratacionesabiertas.idaip.org.mx/contratacionesabiertas/datosabiertos
+    """
+    name = 'mexico_idaip'
+
+    # BaseSpider
+    root_path = 'arrayReleasePackage.item'
+    date_format = 'year'
+    default_from_date = '2020'
+    default_until_date = '2021'
+
+    # SimpleSpider
+    data_type = 'release_package'
+
+    # PeriodicSpider
+    pattern = 'http://74.208.135.52:3000/edca/contractingprocess/{}'
+
+    def get_formatter(self):
+        return components(-1)
