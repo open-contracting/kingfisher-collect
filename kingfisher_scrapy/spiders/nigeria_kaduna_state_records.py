0eb022902482605846bb604175f73673a419abcb
cecicasco <cecilia.casco7@gmail.com>
2021-06-21 15:40:27 -0400

    add base class

---
+++ kingfisher_scrapy/spiders/nigeria_kaduna_state_records.py
@@ -0,0 +1,20 @@
+from kingfisher_scrapy.spiders.nigeria_kaduna_state_base import NigeriaKadunaStateBudeshiBase
+from kingfisher_scrapy.util import components
+
+
+class KadunaStateBudeshiRecords(NigeriaKadunaStateBudeshiBase):
+    """
+    Domain
+        Kaduna State
+    Bulk download documentation
+        https://www.budeshi.ng/kadppa/api
+    """
+    name = 'nigeria_kaduna_state_records'
+
+    # SimpleSpider
+    data_type = 'record_package'
+
+    def build_urls(self, project):
+        id = project['id']
+        url = f'{self.base_url}record/{id}'
+        yield self.build_request(url, formatter=components(-2))
---
+++ kingfisher_scrapy/spiders/nigeria_kaduna_state_releases.py
@@ -0,0 +1,24 @@
+import scrapy
+
+from kingfisher_scrapy.base_spider import SimpleSpider
+from kingfisher_scrapy.spiders.nigeria_kaduna_state_base import NigeriaKadunaStateBudeshiBase
+from kingfisher_scrapy.util import components, handle_http_error
+
+
+class NigeriaKadunaStateBudeshiReleases(NigeriaKadunaStateBudeshiBase):
+    """
+    Domain
+        Kaduna State
+    Bulk download documentation
+        https://www.budeshi.ng/kadppa/api
+    """
+    name = 'nigeria_kaduna_state_releases'
+
+    # SimpleSpider
+    data_type = 'release_package'
+
+    def build_urls(self, project):
+        id = project['id']
+        for tag in ('planning', 'tender', 'award', 'contract'):
+            url = f'{self.base_url}releases/{id}/{tag}'
+            yield self.build_request(url.format(id=project['id'], tag=tag), formatter=components(-2))
