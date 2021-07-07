99ae2cb19f283556aefb9949cb4dd5960b57a11f
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-07-07 16:53:45 -0400

    feat: Extract append_path_components from add_path_components

--- kingfisher_scrapy/util.py
+++ kingfisher_scrapy/util.py
@@ -129,10 +129,18 @@ def replace_parameters(url, **kwargs):
     return parsed._replace(query=urlencode(query, doseq=True)).geturl()
 
 
+def append_path_components(url, path):
+    """
+    Returns a URL after appending path components to its path.
+    """
+    parsed = urlsplit(url)
+    return urljoin(parsed._replace(path=f'{parsed.path}/').geturl(), quote(path.lstrip('/')))
+
+
 def add_query_string(method, params):
     """
-    Returns a function that yields the requests yielded by the wrapped method, after updating their query string
-    parameters' values.
+    Returns a function that yields the requests yielded by the wrapped method, after updating the query string
+    parameter values in each request's URL.
     """
     def wrapper(*args, **kwargs):
         for request in method(*args, **kwargs):
@@ -143,16 +151,12 @@ def add_query_string(method, params):
 
 def add_path_components(method, path):
     """
-    Returns a function that yields the requests yielded by the wrapped method, after updating their path parameters'
-    values.
-
-    :param method: a function that yields requests
-    :param path str: the path to append to the URLs of the yielded requests
+    Returns a function that yields the requests yielded by the wrapped method, after appending path components
+    to each request's URL.
     """
     def wrapper(*args, **kwargs):
         for request in method(*args, **kwargs):
-            parsed = urlsplit(request.url)
-            url = urljoin(parsed._replace(path=f'{parsed.path}/').geturl(), quote(path.lstrip('/')))
+            url = append_path_components(request.url, path)
             yield request.replace(url=url)
     return wrapper
 
