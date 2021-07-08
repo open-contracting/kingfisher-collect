492b39e49081d04f83010c1b6c7795863e647f53
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2021-07-08 09:34:53 -0400

    nigeria_budeshi_releases: change if condition for null releases
    Signed-off-by: Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>

--- kingfisher_scrapy/spiders/nigeria_budeshi_releases.py
+++ kingfisher_scrapy/spiders/nigeria_budeshi_releases.py
@@ -28,7 +28,7 @@ class NigeriaBudeshiReleases(NigeriaBudeshiBase):
         #   "releases": [
         #     null
         #   ]
-        if 'releases' in data and data['releases'] and any(release is None for release in data['releases']):
+        if 'releases' in data and data['releases'] == [None]:
             yield self.build_file_error_from_response(response, errors=data)
         else:
             yield from super().parse(response)
