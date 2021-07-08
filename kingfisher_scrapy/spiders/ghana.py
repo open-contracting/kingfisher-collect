6a807886347565dfa6c146f82033e511dcd95301
cecicasco <cecilia.casco7@gmail.com>
2021-07-08 15:38:02 -0400

    change import

--- kingfisher_scrapy/spiders/ghana.py
+++ kingfisher_scrapy/spiders/ghana.py
@@ -1,6 +1,6 @@
-import scrapy
-
 import datetime
+
+import scrapy
 
 from kingfisher_scrapy.base_spider import CompressedFileSpider
 from kingfisher_scrapy.util import components, handle_http_error, join
