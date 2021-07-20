34ac733733875ac11f615be98a03b8b626a51752
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2021-07-20 15:31:15 -0400

    nigeria_edo_state: custom parse method for numbered keys in releases
    Signed-off-by: Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>

--- kingfisher_scrapy/spiders/nigeria_edo_state.py
+++ kingfisher_scrapy/spiders/nigeria_edo_state.py
@@ -1,12 +1,16 @@
 import scrapy
 
 from kingfisher_scrapy.base_spider import SimpleSpider
+from kingfisher_scrapy.util import handle_http_error
 
 
 class NigeriaEdoState(SimpleSpider):
     """
     Domain
       Edo State Open Contracting Portal - Nigeria
+    Caveats
+      The release list has additional numbered keys with tender, award and contract releases as values. This spider
+      removes the numbered keys and append the releases into the release list
     Bulk download documentation
       http://edpms.edostate.gov.ng/ocds/
     """
@@ -18,3 +22,27 @@ class NigeriaEdoState(SimpleSpider):
     def start_requests(self):
         url = 'http://edpms.edostate.gov.ng/ocds/bulkjson.php'
         yield scrapy.Request(url, meta={'file_name': 'all.json'})
+
+    @handle_http_error
+    def parse(self, response):
+        releases = []
+        data = response.json()
+        # The release list has additional numbered keys with tender, award and contract releases as values, eg:
+        #   "releases": [
+        #            "tender": {
+        #                   ...
+        #             },
+        #             "0": {
+        #                 "ocid": "ocds-fmoaoq-edsg--00546",
+        #                 "id": "ocds-fmoaoq-edsg--00546-02-tender",
+        #             } ]
+        for release in data['releases']:
+            for key in list(release):
+                if key.isnumeric():
+                    # remove the numbered key and append the release into the releases list
+                    releases.append(release[key])
+                    del release[key]
+        data['releases'].extend(releases)
+        yield self.build_file_from_response(response, data=data, data_type=self.data_type, encoding=self.encoding)
+
+
