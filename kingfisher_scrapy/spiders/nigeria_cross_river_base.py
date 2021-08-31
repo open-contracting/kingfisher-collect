a7457d63769a0a0964d0e8dc3b98a36c84a20f62
Ravf95 <ravf95@hotmail.com>
2021-08-31 17:32:05 -0400

    update nigeria cross river spider

--- kingfisher_scrapy/spiders/nigeria_cross_river_base.py
+++ kingfisher_scrapy/spiders/nigeria_cross_river_base.py
@@ -1,16 +1,38 @@
 from abc import abstractmethod
+from datetime import datetime
 
-from kingfisher_scrapy.base_spider import PeriodicSpider
-from kingfisher_scrapy.util import components, join, parameters
+import scrapy
+
+from kingfisher_scrapy.base_spider import SimpleSpider
+from kingfisher_scrapy.util import components, handle_http_error, join, parameters
 
 
-class NigeriaCrossRiverBase(PeriodicSpider):
+class NigeriaCrossRiverBase(SimpleSpider):
     # SimpleSpider
     base_url = 'http://ocdsapi.dppib-crsgov.org/api/ocdsAPI/'
 
     # BaseSpider
     date_format = 'year-month'
-    default_from_date = '2020-02'
+
+    def start_requests(self):
+        url = 'http://ocds.dppib-crsgov.org/Login.aspx?ReturnUrl=%2fReleasePackageViewModel_ListView%2f'
+        yield scrapy.Request(url,
+                             meta={'page': 0},
+                             callback=self.parse_date_list)
+
+    @handle_http_error
+    def parse_date_list(self, response):
+        # pagination handler
+        # page = response.request.meta['page']
+        # available_page_list = [re.findall(r"'(.+?)',?", x) for x in
+        #                       response.xpath('//a[starts-with(@class,"dxp-num")]/@onclick').extract()]
+
+        available_date_list = [datetime.strptime(date.split('T')[0], '%Y-%m-%d') for date in
+                               response.xpath('//tr[@class="dxgvDataRow_XafTheme"]/td[6]/text()').extract()]
+
+        for date in available_date_list:
+            for number, url in enumerate(self.build_urls(date)):
+                yield self.build_request(url, self.get_formatter(), priority=number * -1)
 
     def get_formatter(self):
         return join(components(-1), parameters('year', 'month'))
