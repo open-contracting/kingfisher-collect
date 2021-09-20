5e5387793ab08f6d8501c6d527904476b0123edd
Ravf95 <ravf95@hotmail.com>
2021-09-20 07:05:08 -0400

    update nigeria cross river spider

--- kingfisher_scrapy/spiders/nigeria_cross_river_base.py
+++ kingfisher_scrapy/spiders/nigeria_cross_river_base.py
@@ -1,19 +1,32 @@
 from abc import abstractmethod
+from datetime import datetime
 
-from kingfisher_scrapy.base_spider import PeriodicSpider
-from kingfisher_scrapy.util import components, join, parameters
+import scrapy
+
+from kingfisher_scrapy.base_spider import SimpleSpider
+from kingfisher_scrapy.util import components, handle_http_error, join, parameters
 
 
-class NigeriaCrossRiverBase(PeriodicSpider):
+class NigeriaCrossRiverBase(SimpleSpider):
     # SimpleSpider
     base_url = 'http://ocdsapi.dppib-crsgov.org/api/ocdsAPI/'
 
     # BaseSpider
     date_format = 'year-month'
-    default_from_date = '2020-02'
+    default_from_date = '2021-02'
 
-    # PeriodicSpider
     formatter = staticmethod(join(components(-1), parameters('year', 'month')))
+
+    def start_requests(self):
+        url = 'http://ocdsapi.dppib-crsgov.org/api/ocdsAPI/getAvailableReleasesSummary'
+        yield scrapy.Request(url, meta={'file_name': 'list.json'})
+
+    @handle_http_error
+    def parse(self, response):
+        for item in response.json():
+            date = datetime(item['year'], item['month'], 1)
+            for number, url in enumerate(self.build_urls(date)):
+                yield self.build_request(url, formatter=self.formatter, callback=super().parse, priority=number * -1)
 
     @abstractmethod
     def build_urls(self, date):
