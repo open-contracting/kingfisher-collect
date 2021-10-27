a08a3e4cae63ad74e304a5bb2eedde34b3180b2b
aguilerapy <amaguilera96@gmail.com>
2021-10-27 16:47:21 -0300

    Update mexico_inai_api

--- kingfisher_scrapy/spiders/mexico_inai_api.py
+++ kingfisher_scrapy/spiders/mexico_inai_api.py
@@ -7,17 +7,16 @@ class MexicoINAIAPI(MexicoINAIBase):
       Instituto Nacional de Transparencia, Acceso a la Información y Protección de Datos Personales (INAI)
     Spider arguments
       from_date
-        Download only data from this year onward (YYYY format). Defaults to '2015'.
+        Download only data from this year onward (YYYY format).
+        If ``until_date`` is provided, defaults to '2015'.
       until_date
-        Download only data until this year (YYYY format). Defaults to '2021'.
+        Download only data until this year (YYYY format).
+        If ``from_date`` is provided, defaults to the current year.
     API documentation
       http://contratacionesabiertas.inai.org.mx/contratacionesabiertas/datosabiertos
     """
     name = 'mexico_inai_api'
+    domain_pattern = 'http://contratacionesabiertas.inai.org.mx:3000/{}'
 
     # BaseSpider
     default_from_date = '2015'
-    default_until_date = '2021'
-
-    # PeriodicSpider
-    pattern = 'http://contratacionesabiertas.inai.org.mx:3000/edca/contractingprocess/{}'
