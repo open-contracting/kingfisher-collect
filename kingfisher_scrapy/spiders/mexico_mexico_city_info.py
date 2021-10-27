b970fd51008acfdf2541ec5acf9dfe5824c4c14e
aguilerapy <amaguilera96@gmail.com>
2021-10-27 16:34:37 -0300

    Update mexico info scrapers

--- kingfisher_scrapy/spiders/mexico_mexico_city_info.py
+++ kingfisher_scrapy/spiders/mexico_mexico_city_info.py
@@ -2,25 +2,24 @@ from kingfisher_scrapy.base_spider import browser_user_agent
 from kingfisher_scrapy.spiders.mexico_inai_base import MexicoINAIBase
 
 
-class MexicoMexicoCityINFOCDMX(MexicoINAIBase):
+class MexicoMexicoCityINFO(MexicoINAIBase):
     """
     Domain
       Instituto de Transparencia, Acceso a la Información Pública, Protección de Datos Personales y Rendición de
       Cuentas de la Ciudad de México (INFOCDMX)
     Spider arguments
       from_date
-        Download only data from this year onward (YYYY format). Defaults to '2019'.
+        Download only data from this year onward (YYYY format).
+        If ``until_date`` is provided, defaults to '2019'.
       until_date
-        Download only data until this year (YYYY format). Defaults to '2021'.
+        Download only data until this year (YYYY format).
+        If ``from_date`` is provided, defaults to the current year.
     API documentation
       https://dashboard.infocdmx.org.mx/contratacionesabiertas/datosabiertos
     """
-    name = 'mexico_mexico_city_infocdmx'
+    name = 'mexico_mexico_city_info'
     user_agent = browser_user_agent  # to avoid Internal Server Error
+    domain_pattern = 'https://contratacionesabiertas.infocdmx.org.mx:3000/{}'
 
     # BaseSpider
     default_from_date = '2019'
-    default_until_date = '2021'
-
-    # PeriodicSpider
-    pattern = 'https://contratacionesabiertas.infocdmx.org.mx:3000/edca/contractingprocess/{}'
--- kingfisher_scrapy/spiders/mexico_mexico_state_info.py
+++ kingfisher_scrapy/spiders/mexico_mexico_state_info.py
@@ -1,24 +1,23 @@
 from kingfisher_scrapy.spiders.mexico_inai_base import MexicoINAIBase
 
 
-class MexicoMexicoStateINFOEM(MexicoINAIBase):
+class MexicoMexicoStateINFO(MexicoINAIBase):
     """
     Domain
       Instituto de Transparencia, Acceso a la Información Pública y Protección de Datos Personales del Estado de México
       y Municipios (INFOEM)
     Spider arguments
       from_date
-        Download only data from this year onward (YYYY format). Defaults to '2021'.
+        Download only data from this year onward (YYYY format).
+        If ``until_date`` is provided, defaults to '2021'.
       until_date
-        Download only data until this year (YYYY format). Defaults to '2021'.
+        Download only data until this year (YYYY format).
+        If ``from_date`` is provided, defaults to the current year.
     API documentation
       http://www.infoem.org.mx:4000/contratacionesabiertas/datosabiertos
     """
-    name = 'mexico_mexico_state_infoem'
+    name = 'mexico_mexico_state_info'
+    domain_pattern = 'http://infoem.org.mx:3000/{}'
 
     # BaseSpider
     default_from_date = '2021'
-    default_until_date = '2021'
-
-    # PeriodicSpider
-    pattern = 'http://infoem.org.mx:3000/edca/contractingprocess/{}'
