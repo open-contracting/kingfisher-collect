03589048bb2e3c3fa8cd7dde2c824e588df832af
Ravf95 <ravf95@hotmail.com>
2021-10-29 17:00:25 -0300

    italy spider: add suggestions

--- kingfisher_scrapy/spiders/italy.py
+++ kingfisher_scrapy/spiders/italy.py
@@ -18,8 +18,8 @@ class Italy(SimpleSpider):
 
     def start_requests(self):
         yield scrapy.Request(
+            # From https://www.appaltipop.it/it/download
             'https://www.appaltipop.it/_next/data/LxpUO4Pg-S_nnq33fzaED/it/tenders.json',
-            meta={'file_name': 'tenders.json'},
             callback=self.parse_list
         )
 
@@ -37,7 +37,10 @@ class Italy(SimpleSpider):
         # }
 
         for buyer in data['pageProps']['buyers']:
+            # The first resource in the list is the OCDS JSON, the second one a XLSX file
             resource = buyer['appaltipop:releases/0/buyer/dataSource/resources'][0]
+
+            # The JSON file path looks like 'data/IT-CF-01232710374/ocds.json'
             filepath = resource['appaltipop:releases/0/buyer/resource/url']
             url = f'https://raw.githubusercontent.com/ondata/appaltipop/master/{filepath}'
             yield self.build_request(url, formatter=components(-2))
