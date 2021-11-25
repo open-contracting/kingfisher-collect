f20dee450d875788a4d65fa7ba90486b72e8cb2c
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-11-25 14:43:35 -0500

    build: Use find_packages() to avoid gotcha when creating new modules

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 # This project is not intended to be released or used as a Python package. This file only exists for scrapyd-client.
 # https://github.com/scrapy/scrapyd-client/blob/v1.1.0/README.rst
 
 setup(
-    packages=[
-        'kingfisher_scrapy',
-        'kingfisher_scrapy.spiders',
-    ],
+    packages=find_packages(exclude=['tests', 'tests.*']),
     package_data={
         'kingfisher_scrapy': ['item_schema/*.json'],
     },
