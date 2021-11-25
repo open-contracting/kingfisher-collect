9bea0596847bfb041db6e6efb2db2f2a6968473e
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-11-25 15:04:59 -0500

    chore: Use setup.cfg instead of .flake8

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 # This project is not intended to be released or used as a Python package. This file only exists for scrapyd-client.
 # https://github.com/scrapy/scrapyd-client/blob/v1.1.0/README.rst
