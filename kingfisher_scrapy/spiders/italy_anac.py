66a63af6b1da3093d1e3683c673b63520f83a533
Ravf95 <ravf95@hotmail.com>
2021-11-25 11:58:00 -0300

    refactor: italy anac spider

--- kingfisher_scrapy/spiders/italy_anac.py
+++ kingfisher_scrapy/spiders/italy_anac.py
@@ -1,26 +1,32 @@
-from kingfisher_scrapy.base_spider import PeriodicSpider
-from kingfisher_scrapy.util import components
+import scrapy
+
+from kingfisher_scrapy.base_spider import SimpleSpider
+from kingfisher_scrapy.util import components, handle_http_error
 
 
-class ItalyANAC(PeriodicSpider):
+class ItalyANAC(SimpleSpider):
     """
     Domain
        Italy ANAC
+    API documentation
+        https://dati.anticorruzione.it/opendata/about
     Bulk download documentation
       https://dati.anticorruzione.it/opendata/organization/anticorruzione
     """
     name = 'italy_anac'
     download_timeout = 99999
 
-    # BaseSpider
-    date_format = 'year-month'
-    default_from_date = '2018-01'
-    default_until_date = '2020-12'
-
-    # PeriodicSpider
-    pattern = 'https://dati.anticorruzione.it/' \
-              'opendata/download/dataset/ocds/filesystem/bulk/{0.year:d}/{0.month:02d}.json'
-    formatter = staticmethod(components(-1))
-
     # SimpleSpider
     data_type = 'release_package'
+
+    def start_requests(self):
+        url = 'https://dati.anticorruzione.it/opendata/api/3/action/package_search?q=ocds'
+        yield scrapy.Request(url, meta={'file_name': 'list.json'}, callback=self.parse_list)
+
+    @handle_http_error
+    def parse_list(self, response):
+        data = response.json()
+        for result in data['result']['results']:
+            for resource in result['resources']:
+                if resource['format'].upper() == 'JSON':
+                    yield self.build_request(resource['url'], formatter=components(-2))
