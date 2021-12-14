17651340dabc6ceb3f19a514025cb9f117986db7
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-12-14 13:31:10 -0500

    docs: Use furo

--- docs/conf.py
+++ docs/conf.py
@@ -16,14 +16,14 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath(".."))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'OCDS Kingfisher Collect'
-copyright = '2019, Open Contracting Partnership'
-author = 'Open Contracting Partnership'
+project = "OCDS Kingfisher Collect"
+copyright = "2019, Open Contracting Partnership"
+author = "Open Contracting Partnership"
 
 
 # -- General configuration ---------------------------------------------------
@@ -32,18 +32,17 @@ author = 'Open Contracting Partnership'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx_rtd_theme',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -51,20 +50,18 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "furo"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 
 # -- Extension configuration -------------------------------------------------
 
-# Needed for ReadTheDocs (Sphinx 1.8).
-master_doc = 'index'
-
 autodoc_default_options = {
-    'members': None,
-    'member-order': 'bysource',
+    "members": None,
+    "member-order": "bysource",
 }
+autodoc_typehints = "description"
