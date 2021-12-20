0091c33205cf4b61d1d1b5174a22b1010384a54f
Ravf95 <ravf95@hotmail.com>
2021-12-20 11:34:56 -0300

    add mexico sinaloa ceaip spider

---
+++ kingfisher_scrapy/spiders/mexico_sinaloa_ceaip.py
@@ -0,0 +1,24 @@
+from kingfisher_scrapy.spiders.mexico_inai_base import MexicoINAIBase
+
+
+class MexicoSinaloaCEAIP(MexicoINAIBase):
+    """
+    Domain
+      Comisión Estatal para el Acceso a la Información Pública
+    Spider arguments
+      from_date
+        Download only data from this year onward (YYYY format).
+        If ``until_date`` is provided, defaults to '2021'.
+      until_date
+        Download only data until this year (YYYY format).
+        If ``from_date`` is provided, defaults to the current year.
+    API documentation
+      http://ceaipsinaloa.ddns.net:4000/contratacionesabiertas/datosabiertos
+    """
+    name = 'mexico_sinaloa_ceaip'
+
+    # BaseSpider
+    default_from_date = '2021'
+
+    # MexicoINAIBase
+    base_url = 'http://ceaipsinaloa.ddns.net:3000'
