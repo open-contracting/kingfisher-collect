65b7b5bb7c56a55a1c06ace3c8627d8cb29b84ad
cecicasco <cecilia.casco7@gmail.com>
2021-12-30 20:55:56 -0300

    add handle http error

--- kingfisher_scrapy/spiders/mexico_quien_es_quien_records.py
+++ kingfisher_scrapy/spiders/mexico_quien_es_quien_records.py
@@ -1,5 +1,7 @@
-from kingfisher_scrapy.spiders.mexico_quien_es_quien_base import MexicoQuienEsQuienBase
+from json import dumps
 
+from kingfisher_scrapy.spiders.mexico_quien_es_quien_base import MexicoQuienEsQuienBase
+from kingfisher_scrapy.util import handle_http_error
 
 class MexicoQuienEsQuienRecords(MexicoQuienEsQuienBase):
     """
@@ -17,3 +19,14 @@ class MexicoQuienEsQuienRecords(MexicoQuienEsQuienBase):
 
     # IndexSpider
     base_url = 'https://api.quienesquien.wiki/v3/record'
+
+    @handle_http_error
+    def parse(self, response):
+        # data looks like ...
+        # and should looks like ..
+        data = response.json()
+
+        if 'records' in data['data'][0]:
+            data['data'][0] = data['data'][0]['records']
+            response = response.replace(body=dumps(data))
+        yield self.build_file_from_response(response, data_type=self.data_type)
