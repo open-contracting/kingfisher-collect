d66aa9746c9447635264a00e49ced9643bcd394e
James McKinney <26463+jpmckinney@users.noreply.github.com>
2022-04-08 16:47:03 -0400

    docs: Correct indentation of Ghana docstring

--- kingfisher_scrapy/spiders/ghana.py
+++ kingfisher_scrapy/spiders/ghana.py
@@ -3,15 +3,15 @@ from kingfisher_scrapy.spiders.european_dynamics_base import EuropeanDynamicsBas
 
 class Ghana(EuropeanDynamicsBase):
     """
-       Domain
-         Ghana Electronic Procurement System (GHANEPS)
-       Spider arguments
-          from_date
-            Download only data from this month onward (YYYY-MM format).
-            If ``until_date`` is provided, defaults to '2019-07'.
-          until_date
-            Download only data until this month (YYYY-MM format).
-            If ``from_date`` is provided, defaults to the current month.
+    Domain
+      Ghana Electronic Procurement System (GHANEPS)
+    Spider arguments
+       from_date
+         Download only data from this month onward (YYYY-MM format).
+         If ``until_date`` is provided, defaults to '2019-07'.
+       until_date
+         Download only data until this month (YYYY-MM format).
+         If ``from_date`` is provided, defaults to the current month.
     """
     name = 'ghana'
 
