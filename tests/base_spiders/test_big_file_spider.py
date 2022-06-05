977a5eb2c9f48ac53b2873f44ea3bb0df52bc159
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2022-06-05 19:09:12 -0400

    tests: update BigFileSpider tests

--- tests/base_spiders/test_big_file_spider.py
+++ tests/base_spiders/test_big_file_spider.py
@@ -3,16 +3,33 @@ import json
 import pytest
 
 from kingfisher_scrapy.base_spiders import BigFileSpider
+from kingfisher_scrapy.exceptions import IncoherentConfigurationError
 from kingfisher_scrapy.items import File
 from tests import response_fixture, spider_with_crawler
 
 
-@pytest.mark.parametrize('sample,len_items,len_releases', [(None, 2, 100), (5, 1, 5)])
-def test_parse_release_package(sample, len_items, len_releases):
-    spider = spider_with_crawler(spider_class=BigFileSpider, sample=sample)
-    package = {'releases': []}
+@pytest.mark.parametrize('data_type', ['release', 'record', None, 'other'])
+def test_data_type_invalid(data_type):
+    with pytest.raises(IncoherentConfigurationError) as e:
+        spider_with_crawler(BigFileSpider, data_type=data_type)
+    assert str(e.value) == f"{data_type} data_type is not compatible with BigFileSpider"
+
+
+@pytest.mark.parametrize('data_type', ['release_package', 'record_package'])
+def test_data_type_valid(data_type):
+    # No IncoherentConfigurationError exception.
+    spider_with_crawler(BigFileSpider, data_type=data_type)
+
+
+@pytest.mark.parametrize('sample,len_items,len_releases,data_type', [
+    (None, 2, 100, 'release_package'), (5, 1, 5, 'release_package'), (5, 1, 5, 'record_package')
+])
+def test_parse_package(sample, len_items, len_releases, data_type):
+    spider = spider_with_crawler(spider_class=BigFileSpider, sample=sample, data_type=data_type)
+    key = f'{data_type[-8]}s'
+    package = {key: []}
     for i in range(200):
-        package['releases'].append({'key': 'value'})
+        package[key].append({'key': 'value'})
 
     response = response_fixture(body=json.dumps(package).encode(), meta={'file_name': 'test.json'})
     generator = spider.parse(response)
@@ -22,7 +39,7 @@ def test_parse_release_package(sample, len_items, len_releases):
     assert len(item) == 4
     assert item['file_name'] == 'test.json'
     assert item['url'] == 'http://example.com'
-    assert item['data_type'] == 'release_package'
+    assert item['data_type'] == data_type
     assert item['data']['package'] is not None
     assert item['data']['data'] is not None
 
