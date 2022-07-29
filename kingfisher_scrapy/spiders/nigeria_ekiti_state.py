73b84ab2aad96174926bc367e6b922e2faf67cd8
Ravf95 <ravf95@hotmail.com>
2022-07-29 00:27:34 -0400

    refactor: nigeria_ekiti_state spider

--- kingfisher_scrapy/spiders/nigeria_ekiti_state.py
+++ kingfisher_scrapy/spiders/nigeria_ekiti_state.py
@@ -1,28 +1,13 @@
-import scrapy
-
-from kingfisher_scrapy.base_spiders import SimpleSpider
-from kingfisher_scrapy.util import components, handle_http_error
+from kingfisher_scrapy.spiders.european_dynamics_saas_base import EuropeanDynamicsSassBase
 
 
-class NigeriaEkitiState(SimpleSpider):
+class NigeriaEkitiState(EuropeanDynamicsSassBase):
     """
     Domain
       Nigeria Ekiti State Open Contracting Portal
     """
     name = 'nigeria_ekiti_state'
 
-    # SimpleSpider
-    data_type = 'release_package'
+    # EuropeanDynamicsSassBase
+    base_url = 'https://ocds.bpp.ekitistate.gov.ng/'
 
-    # Local
-    url_prefix = 'https://ocds.bpp.ekitistate.gov.ng/'
-
-    def start_requests(self):
-        url = f'{self.url_prefix}Home/Procurements/'
-        yield scrapy.Request(url, meta={'file_name': 'all.html'}, callback=self.parse_list)
-
-    @handle_http_error
-    def parse_list(self, response):
-        pattern = '//table[@id="datable_3"]/tbody/tr/td[2]/span/a/@href'
-        for item in response.xpath(pattern).re("[0-9]+"):
-            yield self.build_request(f'{self.url_prefix}openapi/packagesapi/{item}', formatter=components(-1))
