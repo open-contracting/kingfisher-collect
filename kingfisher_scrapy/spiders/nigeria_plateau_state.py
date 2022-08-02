691ab495342b7e478b6ccdcb9663aa65182b3c05
Ravf95 <ravf95@hotmail.com>
2022-08-02 16:24:50 -0400

    add nigeria_plateau_state spider

---
+++ kingfisher_scrapy/spiders/nigeria_plateau_state.py
@@ -0,0 +1,17 @@
+import scrapy
+
+from kingfisher_scrapy.base_spiders import SimpleSpider
+
+
+class NigeriaPlateauState(SimpleSpider):
+    """
+    Domain
+      Nigeria Plateau State
+    """
+    name = 'nigeria_plateau_state'
+
+    # SimpleSpider
+    data_type = 'release_package'
+
+    def start_requests(self):
+        yield scrapy.Request('https://plateaustatebpp.com/export', meta={'file_name': 'all.json'})
