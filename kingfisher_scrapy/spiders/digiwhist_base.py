dda296c177ba61868cd5ac9c600cd9137ec8cb43
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2022-10-28 13:48:34 -0300

    isort

--- kingfisher_scrapy/spiders/digiwhist_base.py
+++ kingfisher_scrapy/spiders/digiwhist_base.py
@@ -4,7 +4,7 @@ from io import BytesIO
 import scrapy
 
 from kingfisher_scrapy.base_spiders import BaseSpider
-from kingfisher_scrapy.util import handle_http_error, browser_user_agent
+from kingfisher_scrapy.util import browser_user_agent, handle_http_error
 
 
 class DigiwhistBase(BaseSpider):
