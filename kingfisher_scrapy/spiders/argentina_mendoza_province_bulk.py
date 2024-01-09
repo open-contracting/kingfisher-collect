9b54d09ed4fda4fbe8fd92b0ca957d8147446fea
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2024-01-09 10:19:39 -0300

    feat: add argentina_mendoza_province_bulk

---
+++ kingfisher_scrapy/spiders/argentina_mendoza_province_bulk.py
@@ -0,0 +1,27 @@
+import scrapy
+
+from kingfisher_scrapy.base_spiders import SimpleSpider
+from kingfisher_scrapy.util import components
+
+
+class ArgentinaMendozaProvinceBulk(SimpleSpider):
+    """
+    Domain
+      Gobierno de la Provincia de Mendoza
+    Bulk download documentation
+      https://datosabiertos-compras.mendoza.gov.ar/datasets/
+    """
+    name = 'argentina_mendoza_province_bulk'
+
+    # SimpleSpider
+    data_type = 'release_package'
+
+    base_url = 'https://datosabiertos-compras.mendoza.gov.ar'
+
+    def start_requests(self):
+        yield scrapy.Request(f'{self.base_url}/datasets/', callback=self.parse_list)
+
+    def parse_list(self, response):
+        for file_url in response.xpath('//div/a/@href').getall():
+            if file_url.endswith('.json'):
+                yield self.build_request(f'{self.base_url}{file_url}', formatter=components(-1))
