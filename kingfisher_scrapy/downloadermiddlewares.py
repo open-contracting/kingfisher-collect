bd0d17c475ea18b73ad3c608e4da09bb8f12d48b
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-01-22 21:24:49 -0500

    chore: Move reactor import https://docs.scrapy.org/en/latest/topics/asyncio.html#handling-a-pre-installed-reactor

--- kingfisher_scrapy/downloadermiddlewares.py
+++ kingfisher_scrapy/downloadermiddlewares.py
@@ -2,7 +2,6 @@
 from datetime import datetime
 
 from scrapy.exceptions import IgnoreRequest
-from twisted.internet import reactor
 from twisted.internet.defer import Deferred
 
 
@@ -107,6 +106,8 @@ class DelayedRequestMiddleware:
     def process_request(self, request, spider):
         delay = request.meta.get('wait_time', None)
         if delay:
+            from twisted.internet import reactor
+
             d = Deferred()
             reactor.callLater(delay, d.callback, None)
             return d
