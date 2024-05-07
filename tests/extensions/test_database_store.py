ca4e95b675fa322b953e933b9f88456df57f2d77
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-05-07 14:27:25 -0400

    test: Update DatabaseStore tests for recent fixes

--- tests/extensions/test_database_store.py
+++ tests/extensions/test_database_store.py
@@ -1,6 +1,6 @@
+import datetime
 import logging
 import os
-from datetime import datetime
 from unittest.mock import Mock
 
 import psycopg2
@@ -49,14 +49,19 @@ def test_from_crawler_missing_arguments():
 @pytest.mark.parametrize('from_date,default_from_date,messages', [
     (None, None, ['Getting the date from which to resume the crawl from the test table']),
     (None, '2020-01-01', ['Getting the date from which to resume the crawl from the test table']),
-    ('2020-01-01', None, []),
-    ('2020-01-01', '2020-01-01', []),
+    ('2020-01-01', None, ['Starting crawl from 2020-01-01']),
+    ('2020-01-01', '2020-01-01', ['Getting the date from which to resume the crawl from the test table']),
 ])
 def test_spider_opened_no_resume(cursor, caplog, tmpdir, from_date, default_from_date, messages):
-    spider = spider_with_crawler(crawl_time='2021-05-25T00:00:00',
-                                 settings={'DATABASE_URL': DATABASE_URL, 'FILES_STORE': tmpdir})
-    spider.from_date = from_date
-    spider.default_from_date = default_from_date
+    spider = spider_with_crawler(
+        crawl_time='2021-05-25T00:00:00',
+        from_date=from_date,
+        default_from_date=default_from_date,
+        settings={
+            'DATABASE_URL': DATABASE_URL,
+            'FILES_STORE': tmpdir,
+        },
+    )
 
     extension = DatabaseStore.from_crawler(spider.crawler)
 
@@ -67,14 +72,24 @@ def test_spider_opened_no_resume(cursor, caplog, tmpdir, from_date, default_from
     table_exists = cursor.fetchone()[0]
 
     assert table_exists == 'test'
-    assert spider.from_date == from_date
+    if from_date:
+        assert spider.from_date == datetime.datetime.strptime(from_date, "%Y-%m-%d").replace(
+            tzinfo=datetime.timezone.utc
+        )
+    else:
+        assert spider.from_date == from_date
     assert [record.message for record in caplog.records] == messages
 
 
 @pytest.mark.skipif(SKIP_TEST_IF, reason='KINGFISHER_COLLECT_DATABASE_URL must be set')
 def test_spider_opened_resume(caplog, tmpdir):
-    spider = spider_with_crawler(crawl_time='2021-05-25T00:00:00',
-                                 settings={'DATABASE_URL': DATABASE_URL, 'FILES_STORE': tmpdir})
+    spider = spider_with_crawler(
+        crawl_time='2021-05-25T00:00:00',
+        settings={
+            'DATABASE_URL': DATABASE_URL,
+            'FILES_STORE': tmpdir,
+        },
+    )
     spider.data_type = 'release_package'
 
     extension = DatabaseStore.from_crawler(spider.crawler)
@@ -95,17 +110,22 @@ def test_spider_opened_resume(caplog, tmpdir):
     with caplog.at_level(logging.INFO):
         extension.spider_opened(spider)
 
-    assert spider.from_date == datetime(2021, 5, 26, 0, 0)
+    assert spider.from_date == datetime.datetime.strptime("2021-05-26T10:00:00Z", "%Y-%m-%dT%H:%M:%S%z")
     assert [record.message for record in caplog.records] == [
         'Getting the date from which to resume the crawl from the test table',
-        'Resuming the crawl from 2021-05-26',
+        'Resuming crawl from 2021-05-26',
     ]
 
 
 @pytest.mark.skipif(SKIP_TEST_IF, reason='KINGFISHER_COLLECT_DATABASE_URL must be set')
 def test_spider_closed_warnings(caplog, tmpdir):
-    spider = spider_with_crawler(crawl_time='2021-05-25T00:00:00',
-                                 settings={'DATABASE_URL': DATABASE_URL, 'FILES_STORE': tmpdir})
+    spider = spider_with_crawler(
+        crawl_time='2021-05-25T00:00:00',
+        settings={
+            'DATABASE_URL': DATABASE_URL,
+            'FILES_STORE': tmpdir,
+        },
+    )
     spider.data_type = 'release_package'
     spider.database_store_compile_releases = True
 
@@ -136,7 +156,7 @@ def test_spider_closed_warnings(caplog, tmpdir):
         with caplog.at_level(logging.INFO):
             extension.spider_closed(spider, 'finished')
 
-    assert spider.from_date == datetime(2021, 5, 26, 0, 0)
+    assert spider.from_date == datetime.datetime.strptime("2021-05-26T10:00:00Z", "%Y-%m-%dT%H:%M:%S%z")
     assert [record.message for record in caplog.records] == [
         f'Reading the {tmpdir}/test/20210525_000000 crawl directory with the empty prefix',
         'Creating generator of compiled releases',
@@ -160,8 +180,13 @@ def test_spider_closed_warnings(caplog, tmpdir):
      None),
 ])
 def test_spider_closed(cursor, caplog, tmpdir, data, data_type, sample, compile_releases, table_name):
-    spider = spider_with_crawler(crawl_time='2021-05-25T00:00:00',
-                                 settings={'DATABASE_URL': DATABASE_URL, 'FILES_STORE': tmpdir})
+    spider = spider_with_crawler(
+        crawl_time='2021-05-25T00:00:00',
+        settings={
+            'DATABASE_URL': DATABASE_URL,
+            'FILES_STORE': tmpdir,
+        },
+    )
     spider.data_type = data_type
     spider.sample = sample
     spider.database_store_compile_releases = compile_releases
@@ -217,8 +242,13 @@ def test_spider_closed(cursor, caplog, tmpdir, data, data_type, sample, compile_
 
 @pytest.mark.skipif(SKIP_TEST_IF, reason='KINGFISHER_COLLECT_DATABASE_URL must be set')
 def test_spider_closed_error(caplog, tmpdir):
-    spider = spider_with_crawler(crawl_time='2021-05-25T00:00:00',
-                                 settings={'DATABASE_URL': DATABASE_URL, 'FILES_STORE': tmpdir})
+    spider = spider_with_crawler(
+        crawl_time='2021-05-25T00:00:00',
+        settings={
+            'DATABASE_URL': DATABASE_URL,
+            'FILES_STORE': tmpdir,
+        },
+    )
     extension = DatabaseStore.from_crawler(spider.crawler)
 
     with caplog.at_level(logging.INFO):
