43f66a7b9e68fb0850f8db82cba1e9c74e8791ce
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2024-05-24 11:00:35 -0400

    lint

--- kingfisher_scrapy/spiders/south_africa_national_treasury_api.py
+++ kingfisher_scrapy/spiders/south_africa_national_treasury_api.py
@@ -28,5 +28,6 @@ class SouthAfricaNationalTreasuryAPI(LinksSpider, PeriodicSpider):
     formatter = staticmethod(parameters('PageNumber', 'dateFrom'))
 
     # PeriodicSpider
-    pattern = "https://ocds-api.etenders.gov.za/api/OCDSReleases?dateFrom={0:%Y-%m-%d}&dateTo={1:%Y-%m-%d}&PageNumber=1"
+    pattern = "https://ocds-api.etenders.gov.za/api/OCDSReleases?" \
+              "dateFrom={0:%Y-%m-%d}&dateTo={1:%Y-%m-%d}&PageNumber=1"
     step = 30
