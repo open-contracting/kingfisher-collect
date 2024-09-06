d2207decb5c96f50570032d93a235edf34bdfc49
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2024-09-06 16:31:54 -0400

    closes #1096

--- kingfisher_scrapy/spiders/mexico_mexico_state_infoem.py
+++ kingfisher_scrapy/spiders/mexico_mexico_state_infoem.py
@@ -1,3 +1,5 @@
+import scrapy
+
 from kingfisher_scrapy.spiders.mexico_inai_base import MexicoINAIBase
 
 
@@ -6,20 +8,13 @@ class MexicoMexicoStateINFOEM(MexicoINAIBase):
     Domain
       Instituto de Transparencia, Acceso a la Información Pública y Protección de Datos Personales del Estado de México
       y Municipios (INFOEM)
-    Spider arguments
-      from_date
-        Download only data from this year onward (YYYY format).
-        If ``until_date`` is provided, defaults to '2021'.
-      until_date
-        Download only data until this year (YYYY format).
-        If ``from_date`` is provided, defaults to the current year.
     API documentation
       http://www.infoem.org.mx:4000/contratacionesabiertas/datosabiertos
     """
     name = 'mexico_mexico_state_infoem'
 
-    # BaseSpider
-    default_from_date = '2021'
-
     # MexicoINAIBase
     base_url = 'http://infoem.org.mx:3000'
+
+    def start_requests(self):
+        yield scrapy.Request(f'{self.base_url}/edca/contractingprocess/null', meta={'file_name': 'all.json'})
