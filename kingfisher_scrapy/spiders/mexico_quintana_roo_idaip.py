f58645c4a97b01c6b6033512c1eb1761e82c3c5b
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2024-09-06 16:22:19 -0400

    feat: restore mexico_quitana_roo_idaip ref #989

---
+++ kingfisher_scrapy/spiders/mexico_quintana_roo_idaip.py
@@ -0,0 +1,19 @@
+import scrapy
+
+from kingfisher_scrapy.spiders.mexico_inai_base import MexicoINAIBase
+
+
+class MexicoQuintanaRooIDAIP(MexicoINAIBase):
+    """
+    Domain
+      Instituto de Acceso a la Información y Protección de Datos Personales de Quintana Roo (IDAIPQROO)
+    API documentation
+      http://conab.idaipqroo.org.mx:4000/contratacionesabiertas/datosabiertos
+    """
+    name = 'mexico_quintana_roo_idaip'
+
+    # MexicoINAIBase
+    base_url = 'http://conab.idaipqroo.org.mx:3000'
+
+    def start_requests(self):
+        yield scrapy.Request(f'{self.base_url}/edca/contractingprocess/null', meta={'file_name': 'all.json'})
