073971c4b8855da6d39b08fc803b463529589c01
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-09-10 14:50:51 -0400

    chore: Restore import order

--- kingfisher_scrapy/base_spiders/__init__.py
+++ kingfisher_scrapy/base_spiders/__init__.py
@@ -1,7 +1,11 @@
+# isort:skip_file
+# Avoid circular dependencies at runtime.
+# https://github.com/PyCQA/isort#skip-processing-of-imports-outside-of-configuration
+
 from .base_spider import BaseSpider  # noqa: F401
-from .big_file_spider import BigFileSpider  # noqa: F401
 from .compressed_file_spider import CompressedFileSpider  # noqa: F401
+from .simple_spider import SimpleSpider  # noqa: F401
+from .big_file_spider import BigFileSpider  # noqa: F401
 from .index_spider import IndexSpider  # noqa: F401
 from .links_spider import LinksSpider  # noqa: F401
 from .periodic_spider import PeriodicSpider  # noqa: F401
-from .simple_spider import SimpleSpider  # noqa: F401
