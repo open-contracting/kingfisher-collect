6072d853203d405fa57f7198fed9bfb5c7bd3e9f
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-11-01 11:51:49 -0400

    fix: Disable CookiesMiddleware (tldextract requests a URL that can fail, even though it has a cache at ~/.cache/python-tldextract) https://github.com/john-kurkowski/tldextract

--- kingfisher_scrapy/settings.py
+++ kingfisher_scrapy/settings.py
@@ -32,7 +32,7 @@ CONCURRENT_REQUESTS_PER_DOMAIN = 2  # default 8
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
 TELNETCONSOLE_ENABLED = False
