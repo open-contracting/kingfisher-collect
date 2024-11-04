98eeb4a41e6ceb337bfd3d8240680d9998289181
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-11-04 12:37:30 -0500

    fix(peru_compras): Use new list endpoint, closes #1120 (can confirm that newline fix is still needed)

--- kingfisher_scrapy/spiders/peru_compras.py
+++ kingfisher_scrapy/spiders/peru_compras.py
@@ -1,7 +1,7 @@
 import scrapy
 
 from kingfisher_scrapy.base_spiders import SimpleSpider
-from kingfisher_scrapy.util import handle_http_error, parameters
+from kingfisher_scrapy.util import components, handle_http_error
 
 
 class PeruCompras(SimpleSpider):
@@ -12,44 +12,34 @@ class PeruCompras(SimpleSpider):
         The JSON data sometimes contains unescaped newline characters within strings.
     Spider arguments
       from_date
-        Download only data from this date onward (YYYY-MM-DD format). Defaults to '2017-01-01'.
+        Download only data from this month onward (YYYY-MM format). Defaults to '2021-08'.
       until_date
-        Download only data until this date (YYYY-MM-DD format). Defaults to today.
+        Download only data until this month (YYYY-MM format). Defaults to the current month.
     """
 
     name = 'peru_compras'
 
     # BaseSpider
+    date_format = 'year-month'
+    default_from_date = '2021-08'
     date_required = True
-    default_from_date = '2017-01-01'
 
     # SimpleSpider
     data_type = 'release_package'
 
-    # Local
-    url_prefix = 'https://www.catalogos.perucompras.gob.pe/ConsultaOrdenesPub/'
-
     def start_requests(self):
-        url = f'{self.url_prefix}obtenerFiltros'
-        yield scrapy.Request(url, meta={'file_name': 'list.html'}, callback=self.parse_list)
+        url = 'https://www.catalogos.perucompras.gob.pe/ConsultaOrdenesPub/getListaDescargaMasiva?Anio=&Mes='
+        yield scrapy.Request(url, method='POST', meta={'file_name': 'list.json'}, callback=self.parse_list)
 
     @handle_http_error
     def parse_list(self, response):
         from_date = self.from_date.strftime(self.date_format)
         until_date = self.until_date.strftime(self.date_format)
-        # The response is a large text that looks like list_1¯list_2¯list_3
-        str_lists = response.text.split('¯')
-        # where the first list is the framework agreements list that we need for querying the API
-        # and the items in that list are separated by ¬
-        for framework in str_lists[0].split('¬'):
-            # Each item has the format id-type^description and we need the id for querying the API
-            # e.g.: 130-BIENES^IM-CE-2020-9 MATERIAL MÉDICO ¬128-BIENES^IM-CE-2020-8 DISPOSITIVO MÉDICO IN VITRO ¬
-            framework_id = framework.split('-')[0]
-            if framework_id:
+        for item in response.json():
+            if from_date <= f"{item['C_Anio']}-{item['CodMes']}" <= until_date:
                 yield self.build_request(
-                    f'{self.url_prefix}DescargaJsonOCDS'
-                    f'?pAcuerdo={framework_id}&pFechaIni={from_date}&pFechaFin={until_date}',
-                    formatter=parameters('pAcuerdo')
+                    f"https://saeusceprod01.blob.core.windows.net/contproveedor/DescargaMasiva/{item['C_FileJson']}",
+                    formatter=components(-1)
                 )
 
     @handle_http_error
