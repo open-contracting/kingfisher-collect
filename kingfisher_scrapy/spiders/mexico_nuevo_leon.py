30b3e4411a34682dd0cbe640f8f37ab3bcb7e1b6
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2025-05-05 14:25:54 -0300

    feat(mexico_nuevo_leon): support date filters

--- kingfisher_scrapy/spiders/mexico_nuevo_leon.py
+++ kingfisher_scrapy/spiders/mexico_nuevo_leon.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import scrapy
 
 from kingfisher_scrapy.base_spiders import SimpleSpider
@@ -8,12 +10,22 @@ class MexicoNuevoLeon(SimpleSpider):
     """
     Domain
       Nuevo León - Dirección General de Adquisiciones y Servicios - Secretaría de Administración
+    Spider arguments
+      from_date
+        Download only data from this date onward (YYYY-MM-DD format).
+        If ``until_date`` is provided, defaults to '2024-11-11'.
+      until_date
+        Download only data until this date (YYYY-MM-DD format).
+        If ``from_date`` is provided, defaults to today.
     Bulk download documentation
       https://catalogodatos.nl.gob.mx/dataset/contrataciones-abiertas-direccion-general-de-adquisiciones-y-servicios
     """
 
     name = 'mexico_nuevo_leon'
     user_agent = BROWSER_USER_AGENT  # to avoid HTTP 403
+
+    # BaseSpider
+    default_from_date = '2024-11-11'
 
     # SimpleSpider
     data_type = 'release_package'
@@ -31,4 +43,9 @@ class MexicoNuevoLeon(SimpleSpider):
         for resource in response.json()['result']['resources']:
             # Some files don't include an extension file, so we need to check the file name instead.
             if resource['name'].upper().startswith('JSON-OCDS'):
+                if self.from_date and self.until_date:
+                    date = datetime.strptime(resource['created'], '%Y-%m-%dT%H:%M:%S.%f').replace(
+                        tzinfo=self.from_date.tzinfo)
+                    if not (self.from_date <= date <= self.until_date):
+                        continue
                 yield self.build_request(resource['url'], formatter=components(-1))
