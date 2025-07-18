35a3ec62db4216ce6f121265cb1ed6403b8b46f5
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2025-07-18 17:10:18 -0300

    fix: download portland file from google drive correctly

--- kingfisher_scrapy/spiders/united_states_portland.py
+++ kingfisher_scrapy/spiders/united_states_portland.py
@@ -1,3 +1,5 @@
+from urllib.parse import urlencode
+
 import scrapy
 
 from kingfisher_scrapy.base_spiders import SimpleSpider
@@ -18,9 +20,23 @@ class UnitedStatesPortland(SimpleSpider):
 
     def start_requests(self):
         yield scrapy.Request(
-            # A direct link to the JSON file stored in a Google Drive, bypassing the virus scan warning.
+            # A direct link to the JSON file stored in a Google Drive.
             # The link to the Google Drive folder is published in
             # https://www.portland.gov/business-opportunities/ocds/ocds-data-publication
-            "https://www.googleapis.com/drive/v3/files/10FoGezSloloNP99iWnqUYXZ3mOPuU-Jt?alt=media",
+            "https://drive.google.com/uc?export=download&id=10FoGezSloloNP99iWnqUYXZ3mOPuU-Jt",
+            meta={"file_name": "all.html"},
+            callback=self.parse_response,
+        )
+
+    def parse_response(self, response):
+        # The file is big, so we get the HTML confirmation page with the final link to download the file
+        form = response.xpath('//form[@id="download-form"]')
+        action_url = form.xpath("@action").get()
+        params = {}
+        for key in ("id", "export", "confirm", "uuid"):
+            params[key] = form.xpath(f".//input[@name='{key}']/@value").get()
+
+        yield scrapy.Request(
+            url=f"{action_url}?{urlencode(params)}",
             meta={"file_name": "all.json"},
         )
