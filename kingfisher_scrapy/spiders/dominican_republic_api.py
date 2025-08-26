42ca893f2c1c132a76e8cf3934962dc9279b5497
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2025-08-26 15:24:40 -0300

    feat(dominican_republic_api): update API endpoints and rate limits

--- kingfisher_scrapy/spiders/dominican_republic_api.py
+++ kingfisher_scrapy/spiders/dominican_republic_api.py
@@ -1,61 +1,61 @@
-from json import JSONDecodeError
+import scrapy
 
-from kingfisher_scrapy.base_spiders import LinksSpider, PeriodicSpider
-from kingfisher_scrapy.util import components, handle_http_error
+from kingfisher_scrapy.base_spiders import IndexSpider
+from kingfisher_scrapy.util import handle_http_error, parameters
 
 
-class DominicanRepublicAPI(LinksSpider, PeriodicSpider):
+class DominicanRepublicAPI(IndexSpider):
     """
     Domain
       Dirección General de Contrataciones Públicas (DGCP)
     Spider arguments
       from_date
-        Download only data from this date onward (YYYY-MM-DD format). Defaults to '2018-01-01'.
+        Download only data from this date onward (YYYY-MM-DD format). Defaults to '2015-01-01'.
       until_date
         Download only data until this date (YYYY-MM-DD format). Defaults to today.
-    API documentation
-      https://api.dgcp.gob.do/api/docs
+    Swagger API documentation
+      https://datosabiertos.dgcp.gob.do/api-dgcp/docs/index.html
     """
 
     name = "dominican_republic_api"
+
     custom_settings = {
-        # Reduce the number of concurrent requests to avoid multiple failures.
+        # To avoid hitting the maximum number of request per second too fast
         "CONCURRENT_REQUESTS": 1,
+        # Don't let Scrapy handle error codes.
+        "RETRY_HTTP_CODES": [],
     }
 
     # BaseSpider
-    default_from_date = "2018-01-01"
+    default_from_date = "2015-01-01"
     date_format = "date"
+    date_required = True
+    max_attempts = 3
+    retry_http_codes = [429]
 
     # SimpleSpider
     data_type = "release_package"
 
-    # LinksSpider
-    formatter = staticmethod(components(-2))  # year
-    next_pointer = "/pagination/next"
+    # IndexSpider
+    page_count_pointer = "/pages"
+    parse_list_callback = "parse_page"
 
     # Local
-    base_url = "https://api.dgcp.gob.do/api/"
+    dominican_republic_base_url = "https://datosabiertos.dgcp.gob.do/api-dgcp/v1/ocds/releases"
 
-    # PeriodicSpider
-    pattern = base_url + "date/{0:%Y-%m-%d}/{1:%Y-%m-%d}/1"
+    def start_requests(self):
+        yield scrapy.Request(
+            f"{self.dominican_republic_base_url}/all?start_date={self.from_date.strftime(self.date_format)}"
+            f"&end_date={self.until_date.strftime(self.date_format)}",
+            callback=self.parse_list,
+            meta={"file_name": "page1.json"},
+        )
 
-    def parse(self, response):
-        if response.status == 404:
-            try:
-                data = response.json()
-            except JSONDecodeError:
-                pass
-            else:
-                # API should return an empty package with 200 status.
-                if data == {"msg": "No hay resultados."}:
-                    return
-
-        yield from handle_http_error(DominicanRepublicAPI.parse_success)(self, response)
-
-    def parse_success(self, response):
-        for item in response.json()["data"]:
+    @handle_http_error
+    def parse_page(self, response):
+        # If 404, http status is 200 but content is null
+        for item in response.json()["payload"].get("content") or []:
             yield self.build_request(
-                f"{self.base_url}release/{item['ocid']}", formatter=components(-1), callback=super().parse
+                f"{self.dominican_republic_base_url}?ocid={item['ocid']}",
+                formatter=parameters("ocid"),
             )
-        yield self.next_link(response)
