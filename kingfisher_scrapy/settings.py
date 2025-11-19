50f885f7aac4e62b0cc9f1c23b9e5d88e5024e7c
James McKinney <26463+jpmckinney@users.noreply.github.com>
2025-11-19 00:13:40 -0500

    docs: Update setting defaults for Scrapy 2.13.3

--- kingfisher_scrapy/settings.py
+++ kingfisher_scrapy/settings.py
@@ -27,9 +27,9 @@ CONCURRENT_REQUESTS = 32
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-# DOWNLOAD_DELAY = 3
+# DOWNLOAD_DELAY = 1
 # The download delay setting will honor only one of:
-CONCURRENT_REQUESTS_PER_DOMAIN = 2  # default 8
+CONCURRENT_REQUESTS_PER_DOMAIN = 2  # default 1
 # CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
