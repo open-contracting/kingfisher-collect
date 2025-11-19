a5f4b65d5363554226bcb38aa8c1778ca3bdbbf2
James McKinney <26463+jpmckinney@users.noreply.github.com>
2025-11-19 00:26:52 -0500

    docs: Update settings.py to match Scrapy template

--- kingfisher_scrapy/settings.py
+++ kingfisher_scrapy/settings.py
@@ -14,6 +14,8 @@ BOT_NAME = "kingfisher_scrapy"
 SPIDER_MODULES = ["kingfisher_scrapy.spiders"]
 NEWSPIDER_MODULE = "kingfisher_scrapy.spiders"
 
+ADDONS = {}
+
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 USER_AGENT = "kingfisher_scrapy (+http://www.open-contracting.org)"
@@ -21,16 +23,10 @@ USER_AGENT = "kingfisher_scrapy (+http://www.open-contracting.org)"
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False  # default True
 
-# Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 32
-
-# Configure a delay for requests for the same website (default: 0)
-# See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
-# See also autothrottle settings and docs
-# DOWNLOAD_DELAY = 1
-# The download delay setting will honor only one of:
+# Concurrency and throttling settings
+CONCURRENT_REQUESTS = 32  # default 16
 CONCURRENT_REQUESTS_PER_DOMAIN = 2  # default 1
-# CONCURRENT_REQUESTS_PER_IP = 16
+# DOWNLOAD_DELAY = 1
 
 # Disable cookies (enabled by default)
 COOKIES_ENABLED = False
