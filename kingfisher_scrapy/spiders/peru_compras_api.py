f85ff0caa29d1d0362552b0c7dc2203f6c3de729
James McKinney <26463+jpmckinney@users.noreply.github.com>
2025-11-24 13:30:56 -0500

    chore(peru_compras_api): Reduce single-use variables

--- kingfisher_scrapy/spiders/peru_compras_api.py
+++ kingfisher_scrapy/spiders/peru_compras_api.py
@@ -29,15 +29,12 @@ class PeruComprasAPI(PeruComprasBase):
     def parse_list(self, response):
         from_date = self.from_date.strftime(self.date_format)
         until_date = self.until_date.strftime(self.date_format)
-        # The response is a large text that looks like list_1¯list_2¯list_3
-        str_lists = response.text.split("¯")
-        # where the first list is the framework agreements list that we need for querying the API
-        # and the items in that list are separated by ¬
-        for framework in str_lists[0].split("¬"):
-            # Each item has the format id-type^description and we need the id for querying the API
-            # e.g.: 130-BIENES^IM-CE-2020-9 MATERIAL MÉDICO ¬128-BIENES^IM-CE-2020-8 DISPOSITIVO MÉDICO IN VITRO ¬
-            framework_id = framework.split("-")[0]
-            if framework_id:
+        # The response is a large text that looks like: list_1¯list_2¯list_3
+        # The first list of framework agreements, which we need for querying the API, is "¬"-delimited.
+        for framework in response.text.split("¯")[0].split("¬"):
+            # Each list item has the format id-type^description and we need the id for querying the API:
+            # 130-BIENES^IM-CE-2020-9 MATERIAL MÉDICO ¬128-BIENES^IM-CE-2020-8 DISPOSITIVO MÉDICO IN VITRO ¬
+            if framework_id := framework.split("-")[0]:
                 yield self.build_request(
                     f"{self.url_prefix}DescargaJsonOCDS"
                     f"?pAcuerdo={framework_id}&pFechaIni={from_date}&pFechaFin={until_date}",
