75b33ca1965b6a066b11c94f27bd6868f9d9074f
James McKinney <26463+jpmckinney@users.noreply.github.com>
2026-01-28 13:12:45 -0500

    test: pytest.mark.order no longer needed after 523a247

--- tests/commands/test_updatedocs.py
+++ tests/commands/test_updatedocs.py
@@ -2,8 +2,6 @@ import pytest
 from scrapy.cmdline import execute
 
 
-# tests/extensions/test_kingfisher_process_api.py fails if execute() is already called.
-@pytest.mark.order(-1)
 def test_command(caplog):
     with pytest.raises(SystemExit):
         execute(["scrapy", "updatedocs"])
