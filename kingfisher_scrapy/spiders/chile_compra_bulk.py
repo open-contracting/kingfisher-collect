open-contracting/kingfisher-collect

commit cf7db20972f82025a38a8d336e7d2d432b1696e3

feat: Remove FileError. Rename build_file_error_from_response to log_error_from_response.
- Log JSON exceptions, not the entire JSON text

kingfisher_scrapy/spiders/chile_compra_bulk.py
@@ -1,7 +1,7 @@
 import json
 
 from kingfisher_scrapy.base_spiders import CompressedFileSpider, PeriodicSpider
-from kingfisher_scrapy.items import File, FileError
+from kingfisher_scrapy.items import File
 from kingfisher_scrapy.util import MAX_DOWNLOAD_TIMEOUT, components
 
 # curl -I https://ocds.blob.core.windows.net/ocds/202205.zip
@@ -51,10 +51,15 @@ class ChileCompraBulk(CompressedFileSpider, PeriodicSpider):
           "detail": "error"
         }
         """
-        json_data = json.loads(data)
+        parsed = json.loads(data)
 
-        if json_data.get('status') != 200:
-            json_data['http_code'] = json_data['status']
-            return FileError(file_name=file_name, url=url, errors=json_data)
+        if parsed.get('status') != 200:
+            self.logger.error(
+                'status=%d message=%r request=<GET %s> file_name=%s',
+                parsed['status'],
+                parsed['detail'],
+                url,
+                file_name,
+            )
 
         return File(file_name=file_name, url=url, data_type=None, data=data)

kingfisher_scrapy/base_spiders/base_spider.py
@@ -4,7 +4,7 @@ import datetime
 import scrapy
 
 from kingfisher_scrapy.exceptions import IncoherentConfigurationError, SpiderArgumentError
-from kingfisher_scrapy.items import File, FileError, FileItem
+from kingfisher_scrapy.items import File, FileItem
 from kingfisher_scrapy.util import add_path_components, add_query_string
 
 
@@ -353,16 +353,14 @@ class BaseSpider(scrapy.Spider):
             number=number,
         )
 
-    def build_file_error_from_response(self, response, errors=None):
-        """
-        Return a FileError item to yield, based on the response to a request.
-
-        An ``errors`` keyword argument must be a ``dict``, and should set an ``http_code`` key.
-        """
-        return FileError(
-            file_name=response.request.meta.get('file_name', ''),
-            url=response.request.url,
-            errors=errors or {'http_code': response.status},
+    def log_error_from_response(self, response, *, level='error', status=None, message=''):
+        """Log an error message, based on the response to a request."""
+        getattr(self.logger, level)(
+            'status=%d message=%r request=%s file_name=%s',
+            status or response.status,
+            message,
+            response.request,
+            response.request.meta.get('file_name', ''),
         )
 
     @classmethod

kingfisher_scrapy/base_spiders/index_spider.py
@@ -5,7 +5,6 @@ from jsonpointer import resolve_pointer
 from kingfisher_scrapy import util
 from kingfisher_scrapy.base_spiders import SimpleSpider
 from kingfisher_scrapy.exceptions import IncoherentConfigurationError
-from kingfisher_scrapy.items import FileError
 from kingfisher_scrapy.util import handle_http_error, parameters
 
 
@@ -41,7 +40,7 @@ class IndexSpider(SimpleSpider):
 
     The ``parse_list()`` method parses responses as JSON data. To change the parser of these responses - for example,
     to check for an error response or extract the page count from an HTML page - override the ``parse_list_loader()``
-    method. If this method returns a ``FileError``, then ``parse_list()`` yields it and returns.
+    method. If this method returns ``None``, then ``parse_list()`` returns.
 
     Otherwise, results are yielded from all responses by :meth:`~kingfisher_scrapy.SimpleSpider.parse`. To
     change this method, set a ``parse_list_callback`` class attribute to a method's name as a string.
@@ -101,8 +100,7 @@ class IndexSpider(SimpleSpider):
     @handle_http_error
     def parse_list(self, response):
         data = self.parse_list_loader(response)
-        if isinstance(data, FileError):
-            yield data
+        if data is None:
             return
 
         if not self.base_url:

kingfisher_scrapy/extensions/kingfisher_process_api2.py
@@ -9,7 +9,7 @@ from scrapy.exceptions import NotConfigured
 from yapw import methods
 from yapw.clients import Async
 
-from kingfisher_scrapy.items import FileError, PluckedItem
+from kingfisher_scrapy.items import PluckedItem
 
 
 class Client(Async):
@@ -153,11 +153,11 @@ class KingfisherProcessAPI2:
             self._response_error(spider, 'Failed to close collection', response)
 
     def item_scraped(self, item, spider):
-        """Publish a RabbitMQ message to store the file or file item Kingfisher Process."""
+        """Publish a RabbitMQ message to store the file or file item in Kingfisher Process."""
         if not self.collection_id:
             return
 
-        if isinstance(item, PluckedItem | FileError):
+        if isinstance(item, PluckedItem):
             return
 
         data = {

kingfisher_scrapy/items.py
@@ -1,7 +1,7 @@
 import enum
 import io
 import zipfile
-from typing import Any, TypedDict
+from typing import Any
 
 import pydantic
 import rarfile
@@ -27,10 +27,6 @@ class DataType(str, enum.Enum):
     release = "release"
     record_package = "record_package"
     release_package = "release_package"
-
-
-class Errors(TypedDict):
-    http_code: pydantic.conint(strict=True, ge=100, lt=600)
 
 
 class Resource(pydantic.BaseModel, **base_kwargs):
@@ -61,10 +57,6 @@ class File(DataResource):
 # This doesn't inherit from the File class, because we want isinstance(item, File) to be false for FileItem instances.
 class FileItem(DataResource):
     number: pydantic.conint(strict=True, gt=0)
-
-
-class FileError(Resource):
-    errors: Errors
 
 
 class PluckedItem(pydantic.BaseModel, **base_kwargs):

kingfisher_scrapy/pipelines.py
@@ -49,9 +49,6 @@ class Sample:
         if not spider.sample:
             return item
 
-        # Drop FileError items, so that we keep trying to get data.
-        if not isinstance(item, File | FileItem):
-            raise DropItem(f'Sample: Item is a {type(item).__name__}, not a File or FileItem')
         if self.item_count >= spider.sample:
             spider.crawler.engine.close_spider(spider, 'sample')
             raise DropItem('Sample: Maximum sample size reached')

kingfisher_scrapy/spiders/armenia.py
@@ -41,7 +41,7 @@ class Armenia(LinksSpider):
         # Otherwise, parse the response as usual, then (1) pick a date range and (2) do a binary search within it.
         # This approach assumes that, if two offsets error, then intervening offsets error, too.
         else:
-            yield self.build_file_error_from_response(response)
+            self.log_error_from_response(response)
 
             # If the error occurs on the first request, we have no starting offset.
             if get_parameter_value(response.request.url, 'offset'):
@@ -65,7 +65,7 @@ class Armenia(LinksSpider):
         # If this offset failed and reached a limit, stop.
         elif offset >= start_time or exponent > EXPONENT_LIMIT:
             self.logger.info(f'No offset found after {first_offset:,} within {2 ** EXPONENT_LIMIT} days.')  # noqa: G004
-            yield self.build_file_error_from_response(response)
+            self.log_error_from_response(response)
         # Otherwise, continue.
         else:
             new_offset = min(first_offset + MILLISECONDS_PER_DAY * 2 ** exponent, start_time)

kingfisher_scrapy/spiders/chile_compra_api_base.py
@@ -1,10 +1,9 @@
+import json
 from abc import abstractmethod
 from datetime import date
-from json import JSONDecodeError
 
 from kingfisher_scrapy.base_spiders import IndexSpider, PeriodicSpider
 from kingfisher_scrapy.exceptions import SpiderArgumentError
-from kingfisher_scrapy.items import FileError
 from kingfisher_scrapy.util import components, handle_http_error
 
 
@@ -54,8 +53,7 @@ class ChileCompraAPIBase(IndexSpider, PeriodicSpider):
     @handle_http_error
     def parse(self, response):
         data = self.parse_list_loader(response)
-        if isinstance(data, FileError):
-            yield data
+        if data is None:
             return
 
         # Remove NUL bytes.
@@ -75,8 +73,7 @@ class ChileCompraAPIBase(IndexSpider, PeriodicSpider):
         }
         """
         data = self.parse_list_loader(response)
-        if isinstance(data, FileError):
-            yield data
+        if data is None:
             return
 
         for item in data['data']:
@@ -98,14 +95,13 @@ class ChileCompraAPIBase(IndexSpider, PeriodicSpider):
         """
         try:
             data = response.json()
-        except JSONDecodeError:
-            return self.build_file_error_from_response(
-                response, errors={'http_code': response.status, 'text': response.text}
-            )
+        except json.JSONDecodeError as e:
+            self.log_error_from_response(response, level='exception', message=e)
+            return None
 
         if set(data) == {'detail', 'status'}:
-            data['http_code'] = data['status']
-            return self.build_file_error_from_response(response, errors=data)
+            self.log_error_from_response(response, status=data['status'], message=data['detail'])
+            return None
 
         return data
 

commit 3b2de511ba96bacd769b967cd781494cc77e4205

fix(chile_compra_bulk): Return on error (regression in cf7db20)

kingfisher_scrapy/spiders/chile_compra_bulk.py
@@ -61,5 +61,6 @@ class ChileCompraBulk(CompressedFileSpider, PeriodicSpider):
                 url,
                 file_name,
             )
+            return None
 
         return File(file_name=file_name, url=url, data_type=None, data=data)