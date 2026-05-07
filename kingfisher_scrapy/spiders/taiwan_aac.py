db1258cc6abe5acf1cfac74595d1130cb700c5f7
James McKinney <26463+jpmckinney@users.noreply.github.com>
2026-05-07 12:15:16 -0400

    chore: Avoid using link text which is more brittle

--- kingfisher_scrapy/spiders/taiwan_aac.py
+++ kingfisher_scrapy/spiders/taiwan_aac.py
@@ -1,7 +1,7 @@
 import scrapy
 
 from kingfisher_scrapy.base_spiders import SimpleSpider
-from kingfisher_scrapy.util import parameters
+from kingfisher_scrapy.util import get_parameter_value, parameters, replace_parameters
 
 
 class TaiwanAAC(SimpleSpider):
@@ -17,20 +17,16 @@ class TaiwanAAC(SimpleSpider):
     # SimpleSpider
     data_type = "release_package"
 
-    # Local
-    base_url = "https://gpip.aac.moj.gov.tw/cht/"
-
     async def start(self):
-        yield scrapy.Request(f"{self.base_url}index.php?code=list&ids=6&page=1", callback=self.parse_list)
+        yield scrapy.Request(
+            "https://gpip.aac.moj.gov.tw/cht/index.php?code=list&ids=6&page=1", callback=self.parse_list
+        )
 
     def parse_list(self, response):
-        # The listing page mixes PDF/ODS guidance documents and the OCDS JSON file.
-        # There is only one file at present, but we check all pages in case they add multiple files in the future.
-        # The OCDS file is the only link whose title contains "OCDS".
         for href in response.xpath('//a[contains(@title, "OCDS")]/@href').getall():
-            yield self.build_request(f"{self.base_url}{href}", formatter=parameters("ids"))
-        # On the last page, the 下一頁 ("next page") anchor is rendered with href="#",
-        # so check for a real link before following it.
-        next_page = response.xpath('//a[@title="下一頁" and @href!="#"]/@href').get()
-        if next_page:
-            yield scrapy.Request(response.urljoin(next_page), callback=self.parse_list)
+            yield self.build_request(response.urljoin(href), formatter=parameters("ids"))
+
+        # If the page is out of range, there are no list items.
+        if response.xpath('//div[@class="main"]//li'):
+            page = int(get_parameter_value(response.request.url, "page"))
+            yield scrapy.Request(replace_parameters(response.request.url, page=page + 1), callback=self.parse_list)
