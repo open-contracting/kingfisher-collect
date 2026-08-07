8d9aec39893498d2e0b80039404b7cefeede7795
James McKinney <26463+jpmckinney@users.noreply.github.com>
2026-08-06 23:10:53 -0400

    docs: Add link to settings.py template

--- kingfisher_scrapy/settings.py
+++ kingfisher_scrapy/settings.py
@@ -6,6 +6,8 @@
 #     https://docs.scrapy.org/en/latest/topics/settings.html
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+#
+# https://github.com/scrapy/scrapy/blob/master/scrapy/templates/project/module/settings.py.tmpl#L15
 import logging
 import os
 
