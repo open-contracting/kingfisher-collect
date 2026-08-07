65e6114b32317c10e5aeeacd13a9115e6323f590
James McKinney <26463+jpmckinney@users.noreply.github.com>
2026-08-06 23:23:30 -0400

    chrore(scrapy-lint): Fix SCP05: lambda callback

--- kingfisher_scrapy/settings.py
+++ kingfisher_scrapy/settings.py
@@ -32,7 +32,7 @@ ROBOTSTXT_OBEY = False  # default False, template True
 # Concurrency and throttling settings
 CONCURRENT_REQUESTS = 32  # default 16
 CONCURRENT_REQUESTS_PER_DOMAIN = 2  # default 8, template 1
-DOWNLOAD_DELAY = 0  # default 0, template 1
+# DOWNLOAD_DELAY = 1  # default 0, template 1
 
 # Disable cookies (enabled by default)
 COOKIES_ENABLED = False
